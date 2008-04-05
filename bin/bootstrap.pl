265111d5e42b0d170689305cc829b3eca8cf445d
mark <mark@dreamwidth.org>
2008-04-05 01:50:42 -0700

    * update this to use hg and get our vcv

--- bin/bootstrap.pl
+++ bin/bootstrap.pl
@@ -3,8 +3,8 @@
 use strict;
 
 # check for svn in a known place
-die "Expected svn in /usr/bin ... not found!\n"
-    unless -e '/usr/bin/svn';
+die "Expected hg in /usr/bin ... not found!\n"
+    unless -e '/usr/bin/hg';
 
 # get the right directory
 my $LJHOME = $ENV{'LJHOME'};
@@ -18,12 +18,13 @@ die "Did you already bootstrap?  cvs/vcv exists.\n"
     if -d "$LJHOME/cvs/vcv";
 
 # so now get it
-system( '/usr/bin/svn co http://code.sixapart.com/svn/vcv/trunk/ cvs/vcv' );
-die "Unable to checkout vcv from Six Apart SVN repository.\n"
+# FIXME: make this work over http, not ssh.  not everyone has accounts.  ;)
+system( '/usr/bin/hg -q clone ssh://hg@dwscoalition.org/vcv cvs/vcv' );
+die "Unable to checkout vcv from DWS Coalition repository.\n"
     unless -d "$LJHOME/cvs/vcv" && -e "$LJHOME/cvs/vcv/bin/vcv";
 
 # now get vcv to do the rest for us
-system( 'cvs/vcv/bin/vcv --conf=cvs/multicvs.conf --checkout' );
+#system( 'cvs/vcv/bin/vcv --conf=cvs/multicvs.conf --checkout' );
 
 # finished :-)
 print "Done!  We hope.  :-)\n";
