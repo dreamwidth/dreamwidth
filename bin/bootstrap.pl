c1dbf079e6558aab3a0143afb871a2a05bd87cae
mark <mark@dreamwidth.org>
2008-04-05 17:08:35 -0700

    * make bootstrap even better

--- bin/bootstrap.pl
+++ bin/bootstrap.pl
@@ -15,9 +15,20 @@ chdir( $LJHOME )
 
 # more than likely we don't have vcv, so let's get it
 die "Did you already bootstrap?  cvs/vcv exists.\n"
-    if -d "$LJHOME/cvs/vcv";
+    if -d "$LJHOME/cvs" && -d "$LJHOME/cvs/vcv";
 
-# so now get it
+# if they don't have a CVS dir, we need to get them one and get the code
+unless ( -d "$LJHOME/cvs" ) {
+    print "Seems we need to start at the beginning, fetching dw-free...\n";
+    mkdir( "$LJHOME/cvs" );
+    system( '/usr/bin/hg -q clone http://hg.dwscoalition.org/dw-free cvs/dw-free' );
+    system( "/bin/cp $LJHOME/cvs/dw-free/cvs/multicvs.conf $LJHOME/cvs" );
+
+    die "Something failed...\n"
+        unless -d "$LJHOME/cvs" && -d "$LJHOME/cvs/dw-free" && -e "$LJHOME/cvs/multicvs.conf";
+}
+
+# so now get vcv
 system( '/usr/bin/hg -q clone http://hg.dwscoalition.org/vcv cvs/vcv' );
 die "Unable to checkout vcv from DWS Coalition repository.\n"
     unless -d "$LJHOME/cvs/vcv" && -e "$LJHOME/cvs/vcv/bin/vcv";
