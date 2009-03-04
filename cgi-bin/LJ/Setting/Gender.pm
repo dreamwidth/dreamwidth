4d44f656c4859ac8f4980ebbaf047161b0062f86
exor674 <exor674@dreamwidth.org>
2009-03-04 08:12:59 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=20
    Don't show gender option for non-individual accounts.
    
    Patch by exor674.

--- cgi-bin/LJ/Setting/Gender.pm
+++ cgi-bin/LJ/Setting/Gender.pm
@@ -3,41 +3,48 @@ use base 'LJ::Setting';
 use strict;
 use warnings;
 
-sub tags { qw(gender sex male female boy girl other) }
+sub tags { qw( gender sex male female boy girl other ) }
 
 sub as_html {
-    my ($class, $u, $errs, $args) = @_;
+    my ( $class, $u, $errs, $args ) = @_;
     my $key = $class->pkgkey;
 
     # show the one just posted, else the default one.
-    my $gender = $class->get_arg($args, "gender") ||
-        $u->prop("gender");
+    my $gender = $class->get_arg( $args, "gender" ) ||
+        $u->prop( "gender" );
 
-    return "<label for='${key}gender'>" . $class->ml('.setting.gender.question') . "</label>" .
+    return "<label for='${key}gender'>" . $class->ml( '.setting.gender.question' ) . "</label>" .
         LJ::html_select({ 'name' => "${key}gender", 'id' => '${key}gender', 'class' => 'select', 'selected' => $gender || 'U' },
-                        'F' => LJ::Lang::ml('/manage/profile/index.bml.gender.female'),
-                        'M' => LJ::Lang::ml('/manage/profile/index.bml.gender.male'),
-                        'O' => LJ::Lang::ml('/manage/profile/index.bml.gender.other'),
-                        'U' => LJ::Lang::ml('/manage/profile/index.bml.gender.unspecified') ) .
-                        $class->errdiv($errs, "gender");
+                        'F' => $class->ml( '/manage/profile/index.bml.gender.female' ),
+                        'M' => $class->ml( '/manage/profile/index.bml.gender.male' ),
+                        'O' => $class->ml( '/manage/profile/index.bml.gender.other' ),
+                        'U' => $class->ml( '/manage/profile/index.bml.gender.unspecified' ) ) .
+                        $class->errdiv( $errs, "gender" );
+}
+
+sub should_render {
+    my ( $class, $u ) = @_;
+
+    return $u->is_individual;
 }
 
 sub error_check {
-    my ($class, $u, $args) = @_;
-    my $val = $class->get_arg($args, "gender");
-    $class->errors("gender" => $class->ml('.setting.gender.error.invalid')) unless $val =~ /^[UMFO]$/;
+    my ( $class, $u, $args ) = @_;
+    my $val = $class->get_arg( $args, "gender" );
+    $class->errors( access => $class->ml( '.setting.gender.error.wrongtype' ) ) if $u->is_individual;
+    $class->errors( gender => $class->ml( '.setting.gender.error.invalid' ) ) unless $val =~ /^[UMFO]$/;
     return 1;
 }
 
 sub save {
-    my ($class, $u, $args) = @_;
-    $class->error_check($u, $args);
+    my ( $class, $u, $args ) = @_;
+    $class->error_check( $u, $args );
 
-    my $gen = $class->get_arg($args, "gender");
-    return 1 if $gen eq ($u->prop('gender') || "U");
+    my $gen = $class->get_arg( $args, "gender" );
+    return 1 if $gen eq ( $u->prop( 'gender' ) || "U" );
 
     $gen = "" if $gen eq "U";
-    $u->set_prop("gender", $gen);
+    $u->set_prop( "gender", $gen );
     $u->invalidate_directory_record;
 }
 
