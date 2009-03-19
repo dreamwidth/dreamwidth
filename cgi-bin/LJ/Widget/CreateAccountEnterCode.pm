3e2a3bbeea7e3695c1bfca4c3bf56fd5d07ee1c3
janinedog <janinedog@dreamwidth.org>
2009-03-19 16:19:40 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=373
    These files were accidentally left out on the commit for the new account creation flow.
    
    Patch by janinedog.

---
+++ cgi-bin/LJ/Widget/CreateAccountEnterCode.pm
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+#
+# LJ::Widget::CreateAccountEnterCode
+#
+# This widget contains the form for giving your account creation code.
+#
+# Authors:
+#      Janine Costanzo <janine@netrophic.com>
+#
+# Copyright (c) 2009 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself.  For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package LJ::Widget::CreateAccountEnterCode;
+
+use strict;
+use base qw(LJ::Widget);
+use Carp qw(croak);
+
+sub render_body {
+    my $class = shift;
+    my %opts = @_;
+
+    return "" unless $LJ::USE_ACCT_CODES;
+
+    my $get = $opts{get};
+
+    my $code = $get->{code};
+    my $errors;
+
+    my $error_msg = sub {
+        my ( $key, $pre, $post ) = @_;
+        my $msg = $errors->{$key};
+        return unless $msg;
+        return "$pre $msg $post";
+    };
+
+    # if we're in this widget with a code defined, then it's invalid
+    # rate limit code input; only allow one code every five seconds
+    if ( $code ) {
+        my $ip = LJ::get_remote_ip();
+        if ( LJ::MemCache::get( "invite_code_try_ip:$ip" ) ) {
+            LJ::MemCache::set( "invite_code_try_ip:$ip", 1, 5 );
+            return "<p>" . $class->ml( 'widget.createaccountentercode.error.toofast' ) . "</p>";
+        }
+        LJ::MemCache::set( "invite_code_try_ip:$ip", 1, 5 );
+        $errors->{code} = $class->ml( 'widget.createaccountentercode.error.invalidcode' );
+    }
+
+    my $ret;
+
+    $ret .= "<p>" . $class->ml( 'widget.createaccountentercode.info' ) . "</p>";
+
+    $ret .= "<form method='get' action='$LJ::SITEROOT/create.bml'>";
+    $ret .= "<?standout " . $class->ml( 'widget.createaccountentercode.code' ) . " ";
+    $ret .= LJ::html_text( {
+        name => 'code',
+        value => LJ::ehtml( $code ),
+        size => 21,
+        maxlength => 20,
+    } );
+    $ret .= " " . LJ::html_submit( $class->ml( 'widget.createaccountentercode.btn.proceed' ) );
+    $ret .= $error_msg->( 'code', '<br /><span class="formitemFlag">', '</span>' );
+    $ret .= " standout?>";
+    $ret .= LJ::html_hidden( ssl => $get->{ssl} ) if $get->{ssl};
+    $ret .= "</form>";
+
+    # FIXME: fill in the proper URL for the payment page when we know it
+    $ret .= "<p style='margin-top: 10px;'>" . $class->ml( 'widget.createaccountentercode.pay', { aopts => "href='#'", sitename => $LJ::SITENAMESHORT } ) . "</p>";
+
+    return $ret;
+}
+
+1;
---
+++ cgi-bin/LJ/Widget/CreateAccountUpgrade.pm
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+#
+# LJ::Widget::CreateAccountUpgrade
+#
+# This widget contains information about why a user should get a paid account
+# and how they can get one.
+#
+# Authors:
+#      Janine Costanzo <janine@netrophic.com>
+#
+# Copyright (c) 2009 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself.  For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package LJ::Widget::CreateAccountUpgrade;
+
+use strict;
+use base qw(LJ::Widget);
+use Carp qw(croak);
+
+sub render_body {
+    my $class = shift;
+    my %opts = @_;
+
+    my $ret;
+    $ret .= "<h2>" . $class->ml( 'widget.createaccountupgrade.title' ) . "</h2>";
+    $ret .= "<p class='intro'>" . $class->ml( 'widget.createaccountupgrade.text', { sitename => $LJ::SITENAMESHORT, aopts => "href='$LJ::HELPURL{paidaccountinfo}'" } ) . "</p>";
+    $ret .= $class->start_form;
+    $ret .= $class->html_submit( submit => $class->ml( 'widget.createaccountupgrade.btn.purchase' ) );
+    $ret .= $class->end_form;
+    $ret .= "<p style='margin-top: 10px;'><a href='$LJ::SITEROOT/create/confirm.bml'>" . $class->ml( 'widget.createaccountupgrade.nextstep' ) . "</a></p>";
+
+    return $ret;
+}
+
+sub handle_post {
+    my $class = shift;
+    my $post = shift;
+    my %opts = @_;
+
+    # FIXME: fill in the proper URL for the payment page when we know it
+    return BML::redirect( "$LJ::SITEROOT/" );
+}
+
+1;
