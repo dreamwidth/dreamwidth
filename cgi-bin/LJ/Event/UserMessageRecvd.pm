e2381d6037b0351162832c20f07b611a32688ede
kfk2 <kfk2@dreamwidth.org>
2009-04-05 20:23:24 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=119
    Fix escaping of characters in plain-text PM emails.
    
    Patch by kfk2.

--- cgi-bin/LJ/Event/UserMessageRecvd.pm
+++ cgi-bin/LJ/Event/UserMessageRecvd.pm
@@ -41,7 +41,7 @@ sub _as_email {
     my $vars = {
         user            => $is_html ? ($u->ljuser_display) : ($u->user),
         subject         => $msg->subject,
-        body            => $msg->body,
+        body            => $is_html ? $msg->body : $msg->body_raw ,
         sender          => $is_html ? ($other_u->ljuser_display) : ($other_u->user),
         postername      => $other_u->user,
         sitenameshort   => $LJ::SITENAMESHORT,
