c41dd13e154a0aa39da27160f5a63aa06544902e
kareila <kareila@dreamwidth.org>
2009-06-10 15:23:52 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1295
    Use members/access as appropriate.
    
    Patch by kareila.

--- cgi-bin/LJ/Console/Command/TagPermissions.pm
+++ cgi-bin/LJ/Console/Command/TagPermissions.pm
@@ -10,8 +10,8 @@ sub desc { "Set tagging permission levels for an account." }
 
 sub args_desc { [
                  'community' => "Optional; community to change permission levels for.",
-                 'add level' => "Accounts at this level can add existing tags to entries. One of 'public', 'friends', 'private', or a custom friend group name.",
-                 'control level' => "Accounts at this level can do everything: add, remove, and create new ones. Value is one of 'public', 'friends', 'private', or a custom friend group name.",
+                 'add level' => "Accounts at this level can add existing tags to entries. Value is one of 'public', 'access' (for personal journals), 'members' (for communities), 'private', 'none', or a custom group name.",
+                 'control level' => "Accounts at this level can do everything: add, remove, and create new tags. Value is one of 'public', 'access' (for personal journals), 'members' (for communities), 'private', 'none', or a custom group name.",
                  ] }
 
 sub usage { '[ "for" <community> ] <add level> <control level>' }
@@ -43,15 +43,18 @@ sub execute {
             unless LJ::can_manage($remote, $foru);
 
         ($add, $control) = ($args[2], $args[3]);
+        map { $_ = 'friends' if $_ eq 'members' } ($add, $control);
     } else {
         ($add, $control) = ($args[0], $args[1]);
+        map { $_ = 'friends' if $_ eq 'access' } ($add, $control);
     }
 
     my $validate_level = sub {
         my $level = shift;
         return $level if $level =~ /^(?:private|public|none|friends)$/;
+        return undef if $level =~ /^(?:members|access)$/;
 
-        my $grp = LJ::get_friend_group($foru, { name => $level });
+        my $grp = $foru->trust_groups( name => $level );
         return "group:$grp->{groupnum}" if $grp;
 
         return undef;
@@ -59,7 +62,7 @@ sub execute {
 
     $add = $validate_level->($add);
     $control = $validate_level->($control);
-    return $self->error("Levels must be one of: 'private', 'public', 'friends', or the name of a friends group.")
+    return $self->error("Levels must be one of: 'private', 'public', 'none', 'access' (for personal journals), 'members' (for communities), or the name of a custom group.")
         unless $add && $control;
 
     $foru->set_prop('opt_tagpermissions', "$add,$control");
