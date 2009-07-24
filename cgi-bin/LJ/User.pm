55ec98c761d1dd36a3bb55e20a473b53ad562fd8
foxfirefey <foxfirefey@dreamwidth.org>
2009-07-23 20:25:26 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=1378
    Remove User.pm code that attempts to load nonexistent s1 userprops.
    
    Patch by foxfirefey.

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -8312,18 +8312,11 @@ sub make_journal
     $u->{'_journalbase'} = LJ::journal_base($u->{'user'}, $opts->{'vhost'});
 
     my $eff_view = $LJ::viewinfo{$view}->{'styleof'} || $view;
-    my $s1prop = "s1_${eff_view}_style";
 
     my @needed_props = ("stylesys", "s2_style", "url", "urlname", "opt_nctalklinks",
                         "renamedto",  "opt_blockrobots", "opt_usesharedpic", "icbm",
                         "journaltitle", "journalsubtitle", "external_foaf_url",
                         "adult_content");
-
-    # S2 is more fully featured than S1, so sometimes we get here and $eff_view
-    # is reply/month/entry/res and that means it *has* to be S2--S1 defaults to a
-    # BML page to handle those, but we don't want to attempt to load a userprop
-    # because now load_user_props dies if you try to load something invalid
-    push @needed_props, $s1prop if $eff_view =~ /^(?:calendar|day|friends|lastn)$/;
 
     # preload props the view creation code will need later (combine two selects)
     if (ref $LJ::viewinfo{$eff_view}->{'owner_props'} eq "ARRAY") {
