56eda8163df9c2e09de0b42dc7e7de7bdbcb8a9c
afuna <afuna@dreamwidth.org>
2009-08-08 18:19:56 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1412
    Add layout credit module for crediting layout authors.
    
    Patch by afuna.

--- cgi-bin/LJ/S2Theme/negatives.pm
+++ cgi-bin/LJ/S2Theme/negatives.pm
@@ -2,7 +2,7 @@ package LJ::S2Theme::negatives;
 use base qw( LJ::S2Theme );
 
 sub cats { qw( featured ) }
-sub designer { "phoenixdreaming" }
+sub designer { "phoenix" }
 
 sub layouts { ( "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
