fe64ff74abdd59877db95b94c1f283402473392f
afuna <afuna@dreamwidth.org>
2009-08-22 13:17:56 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=1655
    Negatives color scheme: Blasted Sands, by <user name='zvi'>
    
    Patch by afuna.

--- cgi-bin/LJ/S2Theme/negatives.pm
+++ cgi-bin/LJ/S2Theme/negatives.pm
@@ -6,4 +6,12 @@ sub designer { "phoenix" }
 
 sub layouts { ( "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
+
+
+package LJ::S2Theme::negatives::blastedsands;
+use base qw( LJ::S2Theme::negatives );
+
+sub cats { qw() }
+sub designer { "zvi" }
+
 1;
