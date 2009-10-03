2bb36e385964d6d0d0f307def564f9f476185239
900degrees <900degrees@dreamwidth.org>
2009-10-03 03:03:11 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1893
    Seven themes for Funky Circles: Atomic Orange, Chocolate Rose, Dark Blue, Earthy Green, Industrial Pink, Industrial Teal, Sea Blues
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/funkycircles.pm
+++ cgi-bin/LJ/S2Theme/funkycircles.pm
@@ -7,10 +7,36 @@ sub layout_prop { "layout_type" }
 sub designer { "900degrees" }
 
 
+package LJ::S2Theme::funkycircles::atomicorange;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
+package LJ::S2Theme::funkycircles::chocolaterose;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
+package LJ::S2Theme::funkycircles::darkblue;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
 package LJ::S2Theme::funkycircles::darkpurple;
 use base qw( LJ::S2Theme::funkycircles );
 sub cats { qw( featured base ) }
 
+package LJ::S2Theme::funkycircles::earthygreen;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
+package LJ::S2Theme::funkycircles::industrialpink;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
+package LJ::S2Theme::funkycircles::industrialteal;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
+
+package LJ::S2Theme::funkycircles::seablues;
+use base qw( LJ::S2Theme::funkycircles );
+sub cats { qw( ) }
 
 1;
-
