bbf162e212bcd8e2f73c48be207ead5ecabd55c2
wyntarvox <wyntarvox@dreamwidth.org>
2009-10-16 15:13:56 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1905
    New Drifting theme: Idolatry,  by <user name='zvi'>.
    Screenshot by <user name='piranha'>.
    Patch by wyntarvox.

--- cgi-bin/LJ/S2Theme/drifting.pm
+++ cgi-bin/LJ/S2Theme/drifting.pm
@@ -13,6 +13,12 @@ use base qw( LJ::S2Theme::drifting );
 sub cats { qw( featured ) }
 sub designer { "zvi" }
 
+package LJ::S2Theme::drifting::idolatry;
+use base qw( LJ::S2Theme::drifting );
+
+sub cats { qw( ) }
+sub designer { "zvi" }
+
 package LJ::S2Theme::drifting::lightondark;
 use base qw( LJ::S2Theme::drifting );
 
