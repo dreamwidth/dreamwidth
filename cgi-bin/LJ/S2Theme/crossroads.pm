5c4cf590bc5013ed83836674783a8df909c278ad
900degrees <900degrees@dreamwidth.org>
2010-03-14 08:52:05 -0700

    http://bugs.dwscoalition.org/show_bug.cgi?id=2440
    Cleanup (mmm I love the smell of fresh cleanup in the morning)
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/crossroads.pm
+++ cgi-bin/LJ/S2Theme/crossroads.pm
@@ -9,47 +9,38 @@ sub designer { "branchandroot" }
 package LJ::S2Theme::crossroads::april;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::bittersweet;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::caramel;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::charcoalfire;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::cinnamoncream;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( featured ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::coconut;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::descendingblue;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::lettuce;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( base ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::lilac;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::nnwm2009;
 use base qw( LJ::S2Theme::crossroads );
@@ -59,36 +50,29 @@ sub designer { "zvi" }
 package LJ::S2Theme::crossroads::oldhoney;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::orangejulius;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::persimmon;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::pineneedles;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::sherbert;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::sky;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 package LJ::S2Theme::crossroads::spearmintice;
 use base qw( LJ::S2Theme::crossroads );
 sub cats { qw( ) }
-sub designer { "branchandroot" }
 
 1;
