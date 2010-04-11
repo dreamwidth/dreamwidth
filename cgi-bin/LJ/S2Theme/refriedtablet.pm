603743d75b244f93a2d3a80855475215554cd7ee
kareila <kareila@dreamwidth.org>
2010-04-10 21:58:29 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=2441
    Five new themes for the Refried Tablet layout:
    
    - Refried Jewels, Refried Shallows and Foliage by <user name=dancing_serpent>
    - Blue Tuesday by <user name=forthwritten>
    - Teals by <user name=edgeraven>
    
    Patch and previews by <user name=900degrees>.

--- cgi-bin/LJ/S2Theme/refriedtablet.pm
+++ cgi-bin/LJ/S2Theme/refriedtablet.pm
@@ -8,24 +8,47 @@ sub designer { "zvi" }
 
 package LJ::S2Theme::refriedtablet::autumnnight;
 use base qw( LJ::S2Theme::refriedtablet );
-sub cats { qw(  ) }
+sub cats { qw( ) }
+
+package LJ::S2Theme::refriedtablet::bluetuesday;
+use base qw( LJ::S2Theme::refriedtablet );
+sub cats { qw( ) }
+sub designer { "forthwritten" }
 
 package LJ::S2Theme::refriedtablet::californiaroll;
 use base qw( LJ::S2Theme::refriedtablet );
-sub cats { qw(  ) }
+sub cats { qw( ) }
 
 package LJ::S2Theme::refriedtablet::cherryicing;
 use base qw( LJ::S2Theme::refriedtablet );
-sub cats { qw(  ) }
+sub cats { qw( ) }
+
+package LJ::S2Theme::refriedtablet::foliage;
+use base qw( LJ::S2Theme::refriedtablet );
+sub cats { qw( ) }
+sub designer { "dancing_serpent" }
 
 package LJ::S2Theme::refriedtablet::refriedclassic;
 use base qw( LJ::S2Theme::refriedtablet );
 sub cats { qw( base ) }
 
+package LJ::S2Theme::refriedtablet::refriedjewels;
+use base qw( LJ::S2Theme::refriedtablet );
+sub cats { qw( ) }
+sub designer { "dancing_serpent" }
+
+package LJ::S2Theme::refriedtablet::refriedshallows;
+use base qw( LJ::S2Theme::refriedtablet );
+sub cats { qw( ) }
+sub designer { "dancing_serpent" }
 
 package LJ::S2Theme::refriedtablet::seeded;
 use base qw( LJ::S2Theme::refriedtablet );
 sub cats { qw( featured ) }
 
-1;
+package LJ::S2Theme::refriedtablet::teals;
+use base qw( LJ::S2Theme::refriedtablet );
+sub cats { qw( ) }
+sub designer { "edgeraven" }
 
+1;
