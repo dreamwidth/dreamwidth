6b3926cbc0391c634dfcd4988c13cc5fc5d9a93b
yvi <yvi@dreamwidth.org>
2010-06-14 16:09:44 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=2614
    Update visibility of email checkboxes when notification checkboxes
     change state, on the /manage/subscriptions page.
    
    Patch by yvi.

--- htdocs/js/notifications.js
+++ htdocs/js/notifications.js
@@ -5,6 +5,15 @@ $(document).ready(function()
         event.preventDefault();
         $.post("/__rpc_esn_subs", {action:'delsub', subid:$(this).attr("subid"), auth_token:$(this).attr("auth_token")});
         $(this).closest('tr').remove();
+    })
+
+    $(".SubscriptionInboxCheck").change(function(event)
+    {
+        if ( $(this).is(':checked') ) {
+            $(this).closest('tr').children(".NotificationOptions").css('visibility', 'visible');
+        } else {
+            $(this).closest('tr').children(".NotificationOptions").css('visibility', 'hidden');
+        }
     });
 }
 );
