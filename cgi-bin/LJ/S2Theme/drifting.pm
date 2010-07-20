4a60bbb2635a69b3ca0000c31e9ca018f976f922
kareila <kareila@dreamwidth.org>
2010-07-20 13:28:04 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=2763
    Four new Drifting themes by <user name=timeasmymeasure>.
    
    Patch by <user name=dancing_serpent>. Previews by <user name=900degrees>.

--- cgi-bin/LJ/S2Theme/drifting.pm
+++ cgi-bin/LJ/S2Theme/drifting.pm
@@ -1,5 +1,5 @@
 package LJ::S2Theme::drifting;
-use base qw(LJ::S2Theme);
+use base qw( LJ::S2Theme );
 
 sub cats { qw() }
 sub designer { "Jennie Griner" }
@@ -9,25 +9,31 @@ sub layout_prop { "layout_type" }
 
 package LJ::S2Theme::drifting::chocolatecherry;
 use base qw( LJ::S2Theme::drifting );
-
 sub cats { qw() }
 sub designer { "zvi" }
 
+package LJ::S2Theme::drifting::comfortzone;
+use base qw( LJ::S2Theme::drifting );
+sub cats { qw( ) }
+sub designer { "timeasmymeasure" }
+
+package LJ::S2Theme::drifting::desertme;
+use base qw( LJ::S2Theme::drifting );
+sub cats { qw( ) }
+sub designer { "timeasmymeasure" }
+
 package LJ::S2Theme::drifting::go;
 use base qw( LJ::S2Theme::drifting );
-
 sub cats { qw( ) }
 sub designer { "zvi" }
 
 package LJ::S2Theme::drifting::idolatry;
 use base qw( LJ::S2Theme::drifting );
-
 sub cats { qw() }
 sub designer { "zvi" }
 
 package LJ::S2Theme::drifting::lightondark;
 use base qw( LJ::S2Theme::drifting );
-
 sub cats { qw( ) }
 sub designer { "cesy" }
 
@@ -35,5 +41,15 @@ package LJ::S2Theme::drifting::softblues;
 use base qw( LJ::S2Theme::drifting );
 sub cats { qw( ) }
 sub designer { "ambrya" }
+
+package LJ::S2Theme::drifting::sweetpossibilities;
+use base qw( LJ::S2Theme::drifting );
+sub cats { qw( ) }
+sub designer { "timeasmymeasure" }
+
+package LJ::S2Theme::drifting::winterclarity;
+use base qw( LJ::S2Theme::drifting );
+sub cats { qw( ) }
+sub designer { "timeasmymeasure" }
 
 1;
