82a6f7b08312cd408519e6950242ca4d8b224bc1
fu <fu@dreamwidth.org>
2010-09-14 12:08:18 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=2925
    Stem the flow a little; let people opt out of the global warnings on the dev servers.
    
    Patch by fu.

--- cgi-bin/modperl_subs.pl
+++ cgi-bin/modperl_subs.pl
@@ -26,7 +26,7 @@ use LJ::Config;
 
 BEGIN {
     LJ::Config->load;
-    $^W = 1 if $LJ::IS_DEV_SERVER;
+    $^W = 1 if $LJ::IS_DEV_SERVER && $LJ::ENABLE_GLOBAL_WARNINGS;
 }
 
 use Apache::LiveJournal;
