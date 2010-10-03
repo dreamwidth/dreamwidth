6a93adb4680317952a5e3d550d0a0a8a77d3a35b
900degrees <900degrees@dreamwidth.org>
2010-10-03 21:26:08 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=2796
    CSS and option for a two-columns-left layout for Bases (in addition to the current two-columns-right and one-column)
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/bases.pm
+++ cgi-bin/LJ/S2Theme/bases.pm
@@ -2,7 +2,7 @@ package LJ::S2Theme::bases;
 use base qw( LJ::S2Theme );
 use strict;
 
-sub layouts { ( "1" => "one-column", "2r" => "two-columns-right" ) }
+sub layouts { ( "1" => "one-column", "2l" => "two-columns-left", "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
 
 sub designer { "Malionette" }
