83ec6a2cb370f9af75c95154ca08f2cf3707f924
ninetydegrees <ninetydegrees@dreamwidth.org>
2010-10-12 17:54:22 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=3038
    Add properties, rename images.
    
    Patch by ninetydegrees.

--- cgi-bin/LJ/S2Theme/nouveauoleanders.pm
+++ cgi-bin/LJ/S2Theme/nouveauoleanders.pm
@@ -7,6 +7,44 @@ sub layout_prop { "layout_type" }
 
 sub designer { "branchandroot" }
 
+sub entry_props {
+    my $self = shift;
+    my @props = qw(
+        image_entry_border_group
+        image_entry_border_url
+        image_entry_border_repeat
+        image_entry_border_position
+        image_entry_border_end_odd_group
+        image_entry_border_end_odd_url
+        image_entry_border_end_odd_repeat
+        image_entry_border_end_odd_position
+        image_entry_border_end_even_group
+        image_entry_border_end_even_url
+        image_entry_border_end_even_repeat
+        image_entry_border_end_even_position
+    );
+    return $self->_append_props( "entry_props", @props );
+}
+
+sub comment_props {
+    my $self = shift;
+    my @props = qw(
+        image_comment_border_group
+        image_comment_border_url
+        image_comment_border_repeat
+        image_comment_border_position
+        image_comment_border_end_odd_group
+        image_comment_border_end_odd_url
+        image_comment_border_end_odd_repeat
+        image_comment_border_end_odd_position
+        image_comment_border_end_even_group
+        image_comment_border_end_even_url
+        image_comment_border_end_even_repeat
+        image_comment_border_end_even_position
+    );
+    return $self->_append_props( "comment_props", @props );
+}
+
 package LJ::S2Theme::nouveauoleanders::dustyantique;
 use base qw( LJ::S2Theme::nouveauoleanders );
 sub cats { qw() }
