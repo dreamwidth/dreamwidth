612531cb431e0d0f876a7fe29e239af691ad1e28
fu <fu@dreamwidth.org>
2010-10-12 19:10:40 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=3126
    Use correct method.
    
    Patch by fu.

--- cgi-bin/LJ/S2/ReplyPage.pm
+++ cgi-bin/LJ/S2/ReplyPage.pm
@@ -90,7 +90,7 @@ sub ReplyPage
         $comment = LJ::Comment->new($u, dtalkid => $editid);
         unless ($remote) {
             my $r = $opts->{'r'};
-            my $host = $r->header_in("Host");
+            my $host = $r->headers_in->{Host};
             my $uri = $r->uri;
             my $args = scalar $r->args;
             my $querysep = $args ? "?" : "";
