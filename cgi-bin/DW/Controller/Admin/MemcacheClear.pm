faae4470fb89d6279ccce5a0706ae43e369c2c65
exor674 <exor674@dreamwidth.org>
2010-11-10 20:44:26 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=3212
    Add a tool to clear memcache, available to siteadmins with the proper privs. Also adds a convenience method to sort the keys of a hash by two orders applied one aftera nother.
    
    Patch by exor674.

---
+++ cgi-bin/DW/Controller/Admin/MemcacheClear.pm
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+#
+# DW::Controller::MemcacheClear
+#
+# Clear memcache for a user
+#
+# Authors:
+#      Andrea Nall <anall@andreanall.com>
+#
+# Copyright (c) 2010 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself. For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package DW::Controller::Admin::MemcacheClear;
+
+use strict;
+use warnings;
+use DW::Controller;
+use DW::Routing;
+use DW::Template;
+use DW::Controller::Admin;
+
+use LJ::User;
+use LJ::Userpic;
+
+DW::Routing->register_string( "/admin/memcache_clear", \&index_controller );
+DW::Controller::Admin->register_admin_page( '/',
+    path => 'memcache_clear',
+    ml_scope => '/admin/memcache_clear.tt',
+    privs => [ 'siteadmin:memcacheclear' ]
+);
+
+my %clear = (
+    all => {
+        order => -1,
+        action => sub { LJ::wipe_major_memcache( $_[0] ); },
+    },
+    userpic => {
+        action => sub { LJ::Userpic->delete_cache( $_[0] ); },
+    }
+);
+
+map {
+    $clear{$_}->{key} = $_;
+    $clear{$_}->{name_ml} = ".purge.$_";
+} keys %clear;
+
+sub index_controller {
+    my ( $ok, $rv ) = controller( privcheck => [ "siteadmin:memcacheclear" ] );
+    return $rv unless $ok;
+
+    my $r = DW::Request->get;
+    my $args = $r->post_args || $r->get_args || {};
+
+    my $vars = {
+        %$rv,
+
+        # so the controller looking up the ML strings does not pollute our hash.
+        clear_options => [ map { { %$_ } } values %clear ],
+    };
+
+    if ( $r->method eq 'POST' ) {
+        eval {
+            die "Invalid form auth" unless LJ::check_form_auth( $args->{lj_form_auth} );
+
+            my $u = LJ::load_user( $args->{username} );
+            die "Invalid username" unless $u;
+
+            my $what = $clear{ $args->{what} || 'all' };
+            die "Invalid key" unless $what;
+
+            $what->{action}->( $u );
+            $vars->{cleared} = 1;
+        };
+        if ( $@ ) {
+            $vars->{error} = $@;
+        }
+    }
+
+    return DW::Template->render_template( 'admin/memcache_clear.tt', $vars );
+}
+
+1;
--- cgi-bin/DW/Template/VMethods.pm
+++ cgi-bin/DW/Template/VMethods.pm
@@ -18,15 +18,32 @@ package DW::Template::VMethods;
 use strict;
 use Template::Stash;
 
+my $sort_subs = {
+    alpha => sub { $_[0] cmp $_[1] },
+    numeric => sub { $_[0] <=> $_[1] },
+};
+
 $Template::Stash::LIST_OPS->{ sort_by_key } = sub {
-    my ( $lst, $k, $type ) = @_;
+    my ( $lst, $k, $type, @rest ) = @_;
 
     my @r = ();
-    $type ||= 'alpha';
-    if ( $type eq 'alpha' ) {
-        @r = sort { $a->{$k} cmp $b->{$k} } @$lst; 
-    } elsif ( $type eq 'numeric' ) {
-        @r = sort { $a->{$k} <=> $b->{$k} } @$lst; 
+    my $sb = $sort_subs->{ $type || 'alpha' };
+
+    if ( $type eq 'order' ) {
+        my ( $v_type, $o_ky ) = @rest;
+        $o_ky ||= 'order';
+
+        $sb = $sort_subs->{ $v_type || 'alpha' };
+        return $lst unless defined $sb;
+
+        @r = sort {
+                ( $a->{$o_ky} || 0 ) <=> ( $a->{$o_ky} || 0 ) ||
+                $sb->( $a->{$k}, $b->{$k} )
+            } @$lst;
+    } elsif ( defined $sb ) {
+        @r = sort { $sb->( $a->{$k}, $b->{$k} ) } @$lst;
+    } else {
+        return $lst;
     }
 
     return \@r;
