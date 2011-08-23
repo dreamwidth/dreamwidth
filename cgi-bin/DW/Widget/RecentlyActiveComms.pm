b8fcc6a69fbb8e54543c07e42105b24c79b59231
denise <denise@dreamwidth.org>
2011-08-23 17:32:58 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=1286
    Updated landing page when going to http://www.dreamwidth.org/community: added ten most recently active and recently created communities, help links, links to managing your own communities. Much thanks to <user name="skud"> who did initial work, and <user name="cesy"> who came up with the list of items that should go on the page!
    
    Patch by denise.

---
+++ cgi-bin/DW/Widget/RecentlyActiveComms.pm
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+#
+# DW::Widget::RecentlyActiveComms
+#
+# Returns the 10 most recently updated communities
+#
+# Authors:
+#      Denise Paolucci <denise@dreamwidth.org>
+#
+# Copyright (c) 2011 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself.  For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package DW::Widget::RecentlyActiveComms;
+
+use strict;
+use base qw/ LJ::Widget /;
+
+# hang this widget on the same hook as the (old) stats page uses for
+# determining whether or not to show the newly updated journals
+# section, since the queries are mostly taken from those queries
+# anyway. disable this feature in config.pl if you start having
+# load issues, or if you just don't want this widget to render.
+sub should_render { LJ::is_enabled( 'stats-recentupdates' ) }
+
+sub need_res { qw( stc/widgets/commlanding.css ) }
+
+sub render_body {
+    my ( $class, %opts ) = @_;
+
+    # prep the db reader
+
+    my $dbr = LJ::get_db_reader();
+    my $sth;
+    my $ret;
+
+    # prep the stats we're interested in using here
+
+    $sth = $dbr->prepare( "SELECT u.user, u.name, uu.timeupdate FROM user u, userusage uu WHERE u.userid=uu.userid AND uu.timeupdate > DATE_SUB(NOW(), INTERVAL 30 DAY) AND u.journaltype = 'C' ORDER BY uu.timeupdate DESC LIMIT 10" );
+    $sth->execute;
+
+    $ret .= "<h2>" . $class->ml( 'widget.comms.recentactive' ) . "</h2>";
+    $ret .= "<ul>";
+
+    # build the list
+
+    my $ct;
+    my $targetu;
+
+    while ( my ( $iuser, $iname, $itime ) = $sth->fetchrow_array ) {
+        $targetu = LJ::load_user( $iuser );
+        $ret .= "<li>" . $targetu->ljuser_display . ": " . $iname . ", " . $itime . "</li>\n";
+        $ct++;
+    }
+
+    $ret .= "<li><em> " . BML::ml( 'widget.comms.notavailable' ) . "</em></li>" unless $ct;
+    $ret .= "</ul>\n";
+
+    return $ret;
+}
+
+1;
+
