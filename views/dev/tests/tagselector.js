37566595bee7a45ee851bc9b04739945db9524ef
fu <fu@dreamwidth.org>
2011-12-22 18:02:33 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=2524
    Additional tests for things we know should succeed and fail.
    
    Patch by fu.

--- views/dev/tests/tagselector.js
+++ views/dev/tests/tagselector.js
@@ -11,13 +11,50 @@ test( "no corresponding old function", function() {
 });
 
 module( "jquery" );
-test( "initialize tagselector", function() {
+test( "initialize tagselector", 3, function() {
     $("textarea").tagselector();
-
-    expect(1);
-
     equal($("button").length, 1);
+
+    equal($(".tagselector.ui-widget").length, 0);
+    $("button").click();
+    equal($(".tagselector.ui-widget").length, 1);
+
+    $(".tagselector.ui-widget").hide();
 } );
+
+test( "test tag input", 2, function() {
+    // test data
+    var tags = [
+            "\"something in quotes\"",
+            "#etc","$test","+test",":test",
+            "<strong>abc</strong>",
+            "1337"];
+
+    // we expect that this set of input will break the selector
+    // (so we must fix this at a server level)
+    var breaking = [ 1337 ];
+
+    // FIXME: needs to be more generic
+    var tags_data = { currentCache: "foo",
+                      cache: { "foo": tags, "breaking": breaking },
+                      cachemap: {},
+                      tagslist: [] };
+    $("textarea").data("autocompletewithunknown", tags_data);
+
+    $("textarea").tagselector();
+    $("button").click();
+
+    equal($(".tagselector.ui-widget :checkbox").length, tags.length);
+    $("#tagselector_select").click();   // close the dialog
+
+    raises(function() {
+        tags_data.currentCache = "breaking";
+        $("textarea").tagselector();
+        $("button").click();
+    }, /has no method 'replace'/, "Confirm kind of input that can break the tagselector");
+
+    $(".tagselector.ui-widget").hide();
+});
 
 
 
