2f400b04d14da1069ae75528e9e2d15abb6a1d65
fu <fu@dreamwidth.org>
2011-12-24 13:43:00 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=3986
    Update text to reflect new option name.
    
    Patch by fu.

--- cgi-bin/LJ/Protocol.pm
+++ cgi-bin/LJ/Protocol.pm
@@ -1278,7 +1278,7 @@ sub postevent
         !$time_was_faked && $u->{'newesteventtime'} &&
         $eventtime lt $u->{'newesteventtime'} &&
         !$req->{'props'}->{'opt_backdated'}) {
-        return fail($err, 153, "You have an entry which was posted at $u->{'newesteventtime'}, but you're trying to post an entry before this. Please check the date and time of both entries. If the other entry is set in the future on purpose, edit that entry to use the \"Date Out of Order\" option. Otherwise, use the \"Date Out of Order\" option for this entry instead.");
+        return fail($err, 153, "You have an entry which was posted at $u->{'newesteventtime'}, but you're trying to post an entry before this. Please check the date and time of both entries. If the other entry is set in the future on purpose, edit that entry to use the \"Don't show on Reading Pages\" option. Otherwise, use the \"Don't show on Reading Pages\" option for this entry instead.");
     }
 
     my $qallowmask = $req->{'allowmask'}+0;
