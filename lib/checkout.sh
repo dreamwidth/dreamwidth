fdcc68f05c4576f13bf7079e22acf585894346b1
exor674 <exor674@dreamwidth.org>
2012-05-22 01:44:27 +0000

    (bug 4436) Move LJ-USerSearch to repo.
    There's no other place to get this, and as far as I know
    it is required. Suck it into our repo.
    
    Patch by exor674.

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -1,5 +1,2 @@
-svn checkout http://code.sixapart.com/svn/TheSchwartz/trunk/ TheSchwartz
 svn checkout http://code.livejournal.org/svn/ddlockd/trunk/ ddlockd
-svn checkout http://code.livejournal.org/svn/LJ-UserSearch/trunk/ LJ-UserSearch
-svn checkout http://code.sixapart.com/svn/TheSchwartz-Worker-SendEmail/trunk/ TheSchwartz-Worker-SendEmail
 
