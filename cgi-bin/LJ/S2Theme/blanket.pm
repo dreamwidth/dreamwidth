127e4788f2d24f1990c0e2416feb6b0f12712415
ninetydegrees <ninetydegrees@dreamwidth.org>
2012-08-25 19:56:44 +0800

    Bug 4422. Reorder some options in Blanket

--- cgi-bin/LJ/S2Theme/blanket.pm
+++ cgi-bin/LJ/S2Theme/blanket.pm
@@ -5,10 +5,4 @@ use strict;
 sub layouts { ( "1" => "one-column" ) }
 sub layout_prop { "layout_type" }
 
-sub header_props {
-    my $self = shift;
-    my @props = qw( color_header_footer_border );
-    return $self->_append_props( "header_props", @props );
-}
-
 1;
