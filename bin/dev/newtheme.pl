e67cca68886caa6c293c1c8c7d0632d7f0f9776b
Afuna <fu@dreamwidth.org>
2012-10-02 16:16:06 +0800

    (Bug 4705) Make quotes optional when detecting the layer name

--- bin/dev/newtheme.pl
+++ bin/dev/newtheme.pl
@@ -67,7 +67,7 @@ while ( <STDIN> ) {
 } else {
         next unless $line;
         if ( $line =~ m/^layerinfo / || $line =~ m/ "";/ ) {
-            if ( m/layerinfo "name" = "(.+)"/ ) {
+            if ( m/layerinfo "?name"? = "(.+)"/ ) {
                 $theme_human = $1;
                 ( $theme_name = lc ( $theme_human ) ) =~ s/\s//g;
                 warn "WARNING: $theme_name contains non-ascii characters" if $theme_name =~ m/[^\x01-\x7f]/;
