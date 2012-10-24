105b829a61a5f87ff457be3c4ea6b2973218ebe6
Afuna <fu@dreamwidth.org>
2012-10-24 18:41:43 +0800

    (Bug 4780) Clean up old code

--- htdocs/js/jquery.ajaxtip.js
+++ htdocs/js/jquery.ajaxtip.js
@@ -11,7 +11,6 @@ $.widget("dw.ajaxtip", $.ui.tooltip, {
             collision: "flipfit flipfit"
         },
 
-
         // TODO:
         // allow multiple ajaxtip requests, even if we're not done processing the previous
         multiple: false
@@ -71,7 +70,7 @@ $.widget("dw.ajaxtip", $.ui.tooltip, {
         self.cur_req = deferred;
 
         // now add the throbber. It will be removed automatically
-        $(self.element).throbber(deferred);
+        $(self.element).throbber( deferred );
 
         self.option( "content", function(setContent) {
             deferred.done(function() {
@@ -91,35 +90,13 @@ $.widget("dw.ajaxtip", $.ui.tooltip, {
 /* 3.4k compressed
 $.widget("dw.ajaxtipold", $.ui.tooltip, {
     _create: function() {
-        var tipcontainer = $("<div class='ajaxtooltip ajaxtip' style='display: none'></div>")
-                        .click(function(e) {e.stopPropagation()})
-
-        if ( self.options.persist ) {
-            $(self.element).attr("type", "persistent").bind("mouseout"+self._namespace(), function(e) {
-                self.element.trigger("tooltipout" + self._namespace());
-            } )
-        }
-
         self.element
-            .after(tipcontainer)
             .bind("ajaxresult"+ns, function(e) {
                 var tip = self.element.data("tooltip").getTip()
                      .addClass("ajaxresult-" + e.ajaxresult.status);
                 if ( e.ajaxresult.message ) tip.text(e.ajaxresult.message);
             })
             .tooltip($.extend({
-                predelay: 0,
-                delay: 1500,
-                events: {
-                    // just fade away after a preset period
-                    def       : "ajaxstart"+ns+", tooltipout"+ns+" ajaxresult"+ns,
-                    // persist until the user takes some action (including moving the mouse away from trigger)
-                    persistent: "ajaxstart"+ns+", tooltipout"+ns,
-                    widget    : "ajaxstart"+ns+", ajaxresult"+ns,
-                    tooltip   : "mouseover,mouseleave"
-                },
-                relative: true,
-                effect: "fade",
                 onBeforeShow: function(e) {
                     var tooltipAPI = this;
                     var tip = tooltipAPI.getTip();
@@ -147,9 +124,6 @@ $.widget("dw.ajaxtipold", $.ui.tooltip, {
 
                     tip.css({position: "absolute", top: "", left: ""})
                     self._reposition( tip );
-                },
-                onShow: function(e) {
-                    self._reposition( this.getTip() );
                 }
             },  self.options.tooltip));
     },
@@ -157,79 +131,9 @@ $.widget("dw.ajaxtipold", $.ui.tooltip, {
         if(this.options.content)
             this.element.data("tooltip").show()
     },
-    _reposition: function( tip ) {
-        tip.position({ my: "left top", at: "left bottom", of: this.element, collision: "fit"})
-    },
-
-    widget: function() {
-        return this.element.data("tooltip").getTip();
-    },
-    cancel: function() {
-        var tip = this.element.data("tooltip");
-        if( tip && tip.isShown() ) tip.hide();
-     },
-    show: function() {
-        var tip = this.element.data("tooltip");
-        tip.show();
-        this.success((no msg));
-    },
-    load: function(opts) {
-        var self = this;
-
-        var tip = self.element.data("tooltip");
-        if( tip && ! opts.multiple ) {
-            if( tip.inprogress ) return;
-            if( tip.isShown() ) tip.hide();
-            tip.inprogress = true;
-        }
-
-        self.element.trigger("ajaxstart" + self._namespace());
-
-
-        var xhr = $.ajax({
-            type: opts.formmethod || "POST",
-            url : opts.endpoint ? self._endpointurl( opts.endpoint) : opts.url,
-            data: opts.data,
-            context: opts.context,
-
-            dataType: "json",
-            complete: function() {
-                var tip = self.element.data("tooltip");
-                if ( tip ) {
-                    tip.inprogress = false;
-                    var tipele = tip.getTip();
-                    self._reposition( tipele );
-                    tipele.removeData("xhr");
-                }
-            },
-            success: opts.success,
-            error: opts.error ? opts.error : function( jqxhr, status, error ) {
-                if ( status == "abort" )
-                    this.element.ajaxtip("cancel");
-                else
-                    this.element.ajaxtip( "error", "Error contacting server. " + error);
-
-                this._trigger( "complete" );
-            }
-        });
-
-        if ( tip ) tip.getTip().data( "xhr", xhr );
-    },
-    success: function(msg) {
-        this.element.trigger({ type: "ajaxresult"+this._namespace(),
-                                ajaxresult: { message: msg, status: "success" } })
-    },
-    error: function(msg) {
-        this.element.trigger({ type: "ajaxresult"+this._namespace(),
-                                ajaxresult: { message: msg, status: "error" } })
-    },
-    abort: function(msg) {
-        this.element.data("tooltip").show();
-        this.element.trigger({ type: "ajaxresult"+this._namespace(),
-                                ajaxresult: { message: msg, status: "error" } })
-    }
 });
 */
+// TODO: make this work
 $.extend( $.dw.ajaxtip, {
     closeall: function(e) {
         $(".ajaxtip:visible").each(
