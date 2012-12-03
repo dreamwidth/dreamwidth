d5a332443586dbdfa1976b523b3263339eff94b5
Afuna <fu@dreamwidth.org>
2012-12-03 19:11:17 +0800

    (Bug 4708) Hide the first set of buttons, not the second
    The first set of buttons are duplicates which only show if we have
    filters for the account. The second set of buttons always show. What we
    want is to have the first set of buttons show if there are filters, and
    in addition, if the subscribe/etc are selected (that is, the filters
    could be visible)
    
    Just changes which set of buttons are affected by the JS, logic
    otherwise remains the same.

--- htdocs/js/jquery.circle-add.js
+++ htdocs/js/jquery.circle-add.js
@@ -4,8 +4,8 @@ $("#add_trust, #add_watch").click(function() {
     var $linked = $("."+$this.attr("id"))
     $this.is(":checked") ? $linked.fadeIn() : $linked.fadeOut();
     $("#add_trust, #add_watch").is(":checked")
-        ? $("#add_button_bottom").fadeIn()
-        : $("#add_button_bottom").fadeOut();
+        ? $("#add_button_extra").fadeIn()
+        : $("#add_button_extra").fadeOut();
 })
 $("#add_trust").triggerHandler("click");
 $("#add_watch").triggerHandler("click");
