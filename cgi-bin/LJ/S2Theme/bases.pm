ffe6f2027b828de5f1645de766b1c22fd2856a1c
Momijizukamori <momijizukamori@dreamwidth.org>
2013-01-23 00:43:26 +0000

    (Bug 4740) Fixed potential unsafe color combinations in comment reply form and screened comments

--- cgi-bin/LJ/S2Theme/bases.pm
+++ cgi-bin/LJ/S2Theme/bases.pm
@@ -5,4 +5,9 @@ use strict;
 sub layouts { ( "1" => "one-column", "2l" => "two-columns-left", "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
 
+sub comment_props {
+    my $self = shift;
+    my @props = qw( color_comment_screened );
+    return $self->_append_props( "comment_props", @props );
+}
 1;
