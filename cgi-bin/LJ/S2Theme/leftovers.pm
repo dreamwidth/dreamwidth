e285a6f697a894ef01afb4eab8b6245ca5b13f1a
Momijizukamori <momijizukamori@dreamwidth.org>
2013-03-13 00:05:19 +0000

    (Bug 3714) Property and CSS clean-up

--- cgi-bin/LJ/S2Theme/leftovers.pm
+++ cgi-bin/LJ/S2Theme/leftovers.pm
@@ -5,6 +5,17 @@ use strict;
 sub layouts { ( "1" => "one-column", "1s" => "one-column-split", "2l" => "two-columns-left", "2r" => "two-columns-right", "3" => "three-columns-sides", "3r" => "three-columns-right", "3l" => "three-columns-left" ) }
 sub layout_prop { "layout_type" }
 
+sub archive_props {
+    my $self = shift;
+    my @props = qw(
+    color_calendar_header_background
+    color_calendar_header_text
+    color_calendar_entryday_background
+    color_calendar_entryday_text
+    color_calendar_entryday_link );
+    return $self->_append_props( "archive_props", @props );
+}
+
 sub page_props {
     my $self = shift;
     my @props = qw(color_calender_header_background color_calender_entryday_background color_userpic_border);
