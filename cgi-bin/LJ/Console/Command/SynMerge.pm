086c582ccb5940e89bf0f3ee1cfe8939befe42f7
exor674 <exor674@dreamwidth.org>
2013-06-07 02:59:47 +0000

    (Bug 1716) Refactor synmerge logic into own function

--- cgi-bin/LJ/Console/Command/SynMerge.pm
+++ cgi-bin/LJ/Console/Command/SynMerge.pm
@@ -16,6 +16,7 @@ package LJ::Console::Command::SynMerge;
 use strict;
 use base qw(LJ::Console::Command);
 use Carp qw(croak);
+use LJ::Syn;
 
 sub cmd { "syn_merge" }
 
@@ -46,92 +47,8 @@ sub execute {
     return $self->error("Fourth argument must be 'using'.")
         if $using ne 'using';
 
-    my $from_u = LJ::load_user($from_user)
-        or return $self->error("Invalid user: '$from_user'.");
-
-    my $to_u = LJ::load_user($to_user)
-        or return $self->error("Invalid user: '$to_user'.");
-
-    return $self->error( "Trying to merge into yourself: '$to_user'." )
-        if $from_u->equals( $to_u );
-
-    # we don't want to unlimit this, so reject if we have too many users
-    my @ids = $from_u->watched_by_userids( limit => $LJ::MAX_WT_EDGES_LOAD+1 );
-    return $self->error( "Unable to merge feeds. Too many users are watching the feed '" . $from_u->user . "'. We only allow merges for feeds with at most $LJ::MAX_WT_EDGES_LOAD watchers." )
-        if scalar @ids > $LJ::MAX_WT_EDGES_LOAD;
-
-    foreach ($to_u, $from_u) {
-        return $self->error("Invalid user: '" . $_->user . "' (statusvis is " . $_->statusvis . ", already merged?)")
-            unless $_->is_visible;
-
-        return $self->error($_->user . " is not a syndicated account.")
-            unless $_->is_syndicated;
-    }
-
-    $url = LJ::CleanHTML::canonical_url($url)
-        or return $self->error("Invalid URL.");
-
-    my $dbh = LJ::get_db_writer();
-
-    my $from_oldurl = $dbh->selectrow_array("SELECT synurl FROM syndicated WHERE userid=?", undef, $from_u->id);
-    my $to_oldurl = $dbh->selectrow_array("SELECT synurl FROM syndicated WHERE userid=?", undef, $to_u->id);
-
-    # 1) set up redirection for 'from_user' -> 'to_user'
-    $from_u->update_self( { journaltype => 'R', statusvis => 'R' } );
-    $from_u->set_prop("renamedto", $to_user)
-        or return $self->error("Unable to set userprop.  Database unavailable?");
-
-    # 2) delete the row in the syndicated table for the user
-    #    that is now renamed
-    $dbh->do("DELETE FROM syndicated WHERE userid=?",
-             undef, $from_u->id);
-    return $self->error("Database Error: " . $dbh->errstr)
-        if $dbh->err;
-
-    # 3) update the url of the destination syndicated account and
-    #    tell it to check it now
-    $dbh->do("UPDATE syndicated SET synurl=?, checknext=NOW() WHERE userid=?",
-             undef, $url, $to_u->id);
-    return $self->error("Database Error: " . $dbh->errstr)
-        if $dbh->err;
-
-    # 4) make users who watch 'from_user' now watch 'to_user'
-    # we can't just use delete_ and add_ edges, because we would lose
-    # custom group and colors data
-    if ( @ids ) {
-        # update ignore so we don't raise duplicate key errors
-        $dbh->do( 'UPDATE IGNORE wt_edges SET to_userid=? WHERE to_userid=?',
-              undef, $to_u->id, $from_u->id );
-        return $self->error("Database Error: " . $dbh->errstr)
-            if $dbh->err;
-
-        # in the event that some rows in the update above caused a duplicate key error,
-        # we can delete the rows that weren't updated, since they don't need to be
-        # processed anyway
-        $dbh->do( "DELETE FROM wt_edges WHERE to_userid=?", undef, $from_u->id );
-        return $self->error("Database Error: " . $dbh->errstr)
-            if $dbh->err;
-
-        # clear memcache keys
-        foreach my $id ( @ids ) {
-            LJ::memcache_kill( $id, 'wt_edges' );
-            LJ::memcache_kill( $id, 'wt_list' );
-            LJ::memcache_kill( $id, 'watched' );
-        }
-
-        LJ::memcache_kill( $from_u->id, 'wt_edges_rev' );
-        LJ::memcache_kill( $from_u->id, 'watched_by' );
-
-        LJ::memcache_kill( $to_u->id, 'wt_edges_rev' );
-        LJ::memcache_kill( $to_u->id, 'watched_by' );
-    }
-
-    # log to statushistory
-    my $remote = LJ::get_remote();
-    my $msg = "Merged $from_user to $to_user using URL: $url.";
-    LJ::statushistory_add($from_u, $remote, 'synd_merge', $msg . " Old URL was $from_oldurl.");
-    LJ::statushistory_add($to_u, $remote, 'synd_merge', $msg . " Old URL was $to_oldurl.");
-
+    my ($ok,$msg) = LJ::Syn::merge_feed( from_name => $from_user, to_name => $to_user, url => $url );
+    return $self->error($msg) unless $ok;
     return $self->print($msg);
 }
 
--- cgi-bin/LJ/Syn.pm
+++ cgi-bin/LJ/Syn.pm
@@ -64,4 +64,139 @@ sub _get_feeds_from_db {
     return $popsyn;
 }
 
+=head2 C<< LJ::Syn::merge( %opts ) >>
+
+=over
+
+=item Opts:
+
+=over
+
+=item from - Merge from: LJ::User or userid
+
+=item from_name - Merge from username
+
+=item to - Merge to LJ::User or userid
+
+=item to_name - Merge to username
+
+=item url - Merge to URL
+
+=item pretend - Do not actually merge
+
+=back
+
+=back
+
+=cut
+
+sub merge_feed {
+    my %args = @_;
+    my $from_u;
+    if ( $args{from_name} ) {
+        $from_u = LJ::load_user( $args{from_name} )
+            or return (0, "Invalid user: '" . $args{from_name} . "'.");
+    } else {
+        $from_u = LJ::want_user( $args{from} )
+            or return (0, "Invalid from user.");
+    }
+
+    my $to_u;
+    if ( $args{to_name} ) {
+        $to_u = LJ::load_user( $args{to_name} )
+            or return (0, "Invalid user: '" . $args{to_name} . "'.");
+    } else {
+        $to_u = LJ::want_user( $args{to} )
+            or return (0, "Invalid to user.");
+    }
+
+    return (0, "Trying to merge into yourself." )
+        if $from_u->equals( $to_u );
+
+    # we don't want to unlimit this, so reject if we have too many users
+    my @ids = $from_u->watched_by_userids( limit => $LJ::MAX_WT_EDGES_LOAD+1 );
+    return (0, "Unable to merge feeds. Too many users are watching the feed '" . $from_u->user . "'. We only allow merges for feeds with at most $LJ::MAX_WT_EDGES_LOAD watchers." )
+        if scalar @ids > $LJ::MAX_WT_EDGES_LOAD;
+
+    foreach ($to_u, $from_u) {
+        return (0, "Invalid user: '" . $_->user . "' (statusvis is " . $_->statusvis . ", already merged?)")
+            unless $_->is_visible;
+
+        return (0, $_->user . " is not a syndicated account.")
+            unless $_->is_syndicated;
+    }
+
+    my $url = LJ::CleanHTML::canonical_url( $args{url} )
+        or return (0, "Invalid URL.");
+
+    # Make sure URL is one of the two
+    my $dbh = LJ::get_db_writer();
+    my $from_oldurl = $dbh->selectrow_array("SELECT synurl FROM syndicated WHERE userid=?", undef, $from_u->id);
+    my $to_oldurl = $dbh->selectrow_array("SELECT synurl FROM syndicated WHERE userid=?", undef, $to_u->id);
+
+    my $id_url = $dbh->selectrow_array("SELECT userid FROM syndicated WHERE synurl=?", undef, $url);
+
+    return (0,"URL is in use by another feed") unless $id_url eq $from_u->id || $id_url eq $to_u->id;
+
+    return (1,"Everything seems okay") if $args{pretend};
+
+    # 1) set up redirection for 'from_user' -> 'to_user'
+    $from_u->update_self( { journaltype => 'R', statusvis => 'R' } );
+    $from_u->set_prop("renamedto", $to_u->user)
+        or return (0,"Unable to set userprop.  Database unavailable?");
+
+    # 2) delete the row in the syndicated table for the user
+    #    that is now renamed
+    $dbh->do("DELETE FROM syndicated WHERE userid=?",
+             undef, $from_u->id);
+    return (0,"Database Error: " . $dbh->errstr)
+        if $dbh->err;
+
+    # 3) update the url of the destination syndicated account and
+    #    tell it to check it now
+    $dbh->do("UPDATE syndicated SET synurl=?, checknext=NOW() WHERE userid=?",
+             undef, $url, $to_u->id);
+    return (0,"Database Error: " . $dbh->errstr)
+        if $dbh->err;
+
+    # 4) make users who watch 'from_user' now watch 'to_user'
+    # we can't just use delete_ and add_ edges, because we would lose
+    # custom group and colors data
+    if ( @ids ) {
+        # update ignore so we don't raise duplicate key errors
+        $dbh->do( 'UPDATE IGNORE wt_edges SET to_userid=? WHERE to_userid=?',
+              undef, $to_u->id, $from_u->id );
+        return (0,"Database Error: " . $dbh->errstr)
+            if $dbh->err;
+
+        # in the event that some rows in the update above caused a duplicate key error,
+        # we can delete the rows that weren't updated, since they don't need to be
+        # processed anyway
+        $dbh->do( "DELETE FROM wt_edges WHERE to_userid=?", undef, $from_u->id );
+        return (0,"Database Error: " . $dbh->errstr)
+            if $dbh->err;
+
+        # clear memcache keys
+        foreach my $id ( @ids ) {
+            LJ::memcache_kill( $id, 'wt_edges' );
+            LJ::memcache_kill( $id, 'wt_list' );
+            LJ::memcache_kill( $id, 'watched' );
+        }
+
+        LJ::memcache_kill( $from_u->id, 'wt_edges_rev' );
+        LJ::memcache_kill( $from_u->id, 'watched_by' );
+
+        LJ::memcache_kill( $to_u->id, 'wt_edges_rev' );
+        LJ::memcache_kill( $to_u->id, 'watched_by' );
+    }
+
+    # log to statushistory
+    my $remote = LJ::get_remote();
+    my $msg = "Merged " . $from_u->user . " to " . $to_u->user . " using URL: $url.";
+    LJ::statushistory_add($from_u, $remote, 'synd_merge', $msg . " Old URL was $from_oldurl.");
+    LJ::statushistory_add($to_u, $remote, 'synd_merge', $msg . " Old URL was $to_oldurl.");
+
+    return (1,$msg);
+}
+
 1;
