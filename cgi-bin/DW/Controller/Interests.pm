050b8b0b39b25485f4dbc16004bd95dd452fab4c
Sophie Hamilton <sophie-dw@theblob.org>
2014-06-25 10:32:34 +0000

    (Issue #752) Stylistic fixes, add HTTP_GONE
    A few stylistic fixes made after feedback, and replacing numeric HTTP
    codes with their DW::Request equivalent. I had to add HTTP_GONE as it
    wasn't already in there.

--- cgi-bin/DW/Controller/Interests.pm
+++ cgi-bin/DW/Controller/Interests.pm
@@ -5,13 +5,9 @@
 # Outputs an account's interests in JSON format.
 #
 # Authors:
-#      Thomas Thurman <thomas@thurman.org.uk>
-#      foxfirefey <skittisheclipse@gmail.com>
-#      Mark Smith <mark@dreamwidth.org>
-#      Andrea Nall <anall@andreanall.com>
 #      Sophie Hamilton <sophie-dw@theblob.org>
 #
-# Copyright (c) 2009 by Dreamwidth Studios, LLC.
+# Copyright (c) 2014 by Dreamwidth Studios, LLC.
 #
 # This program is free software; you may redistribute it and/or modify it under
 # the same terms as Perl itself. For a copy of the license, please reference
@@ -21,7 +17,6 @@
 package DW::Controller::Interests;
 
 use strict;
-use warnings;
 use DW::Routing;
 use DW::Request;
 use LJ::JSON;
@@ -36,7 +31,7 @@ sub interests_handler {
     my $opts = shift;
     my $r = DW::Request->get;
 
-    my $format = $formats->{ $opts->format };
+    my $format = $formats->{$opts->format};
 
     # Outputs an error message
     my $error_out = sub {
@@ -48,29 +43,29 @@ sub interests_handler {
     };
 
     # Load the account or error
-    return $error_out->(404, 'Need account name as user parameter') unless $opts->username;
+    return $error_out->( $r->NOT_FOUND, 'Need account name as user parameter' ) unless $opts->username;
     my $u = LJ::load_user_or_identity( $opts->username )
-        or return $error_out->( 404, "invalid account");
+        or return $error_out->( $r->NOT_FOUND, "invalid account");
 
     # Check for other conditions
-    return $error_out->( 410, 'expunged' ) if $u->is_expunged;
-    return $error_out->( 403, 'suspended' ) if $u->is_suspended;
-    return $error_out->( 404, 'deleted' ) if $u->is_deleted;
+    return $error_out->( $r->HTTP_GONE, 'expunged' ) if $u->is_expunged;
+    return $error_out->( $r->FORBIDDEN, 'suspended' ) if $u->is_suspended;
+    return $error_out->( $r->NOT_FOUND, 'deleted' ) if $u->is_deleted;
 
     # deal with renamed accounts
     my $renamed_u = $u->get_renamed_user;
     unless ( $renamed_u && $u->equals( $renamed_u ) ) {
-        $r->header_out("Location", $renamed_u->journal_base . "/data/interests");
+        $r->header_out( "Location", $renamed_u->journal_base . "/data/interests" );
         $r->status( $r->REDIRECT );
         $r->print( to_json( { error => 'moved', moved_to => $renamed_u->journal_base . "/data/interests" } ) );
         return $r->OK;
     }
 
     # Load the interests
-    my $interests = $u->get_interests() || [];
+    my $interests = $u->get_interests || [];
     my $output = {};
-    foreach my $int ( @{ $interests } ) {
-        $output->{ $int->[0] } = {
+    foreach my $int ( @{$interests} ) {
+        $output->{$int->[0]} = {
             interest => $int->[1],
             count    => $int->[2],
         };
