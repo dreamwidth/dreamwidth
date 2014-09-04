f31857b12a40a4dadc7f0d5899bdae2b4bd099aa
Afuna <fu@dreamwidth.org>
2014-09-04 20:12:39 +0800

    [#665] Access component

--- htdocs/js/pages/entry/new.js
+++ htdocs/js/pages/entry/new.js
@@ -3,7 +3,7 @@ var postForm = (function($) {
         $form.collapse({ endpointUrl: "/__rpc_entryformcollapse" });
     };
 
-    var initCurrents = function(moodpics) {
+    var initCurrents = function($form, moodpics) {
         var $moodSelect = $("#js-current-mood");
         var $customMood = $("#js-current-mood-other");
 
@@ -65,6 +65,125 @@ var postForm = (function($) {
         }
     };
 
+    function initAccess($form, formData) {
+        var $custom_access_groups = $("#js-custom-access-groups");
+        var $custom_access_group_members = $("#js-custom-access-group-members");
+        $custom_access_groups.hide();
+
+        var rememberInitialValue = !formData.did_spellcheck;
+        $("#js-security").change( function(e, init) {
+            var $this = $(this);
+            if ( $this.val() == "custom" ) {
+                $custom_access_groups.slideDown();
+            } else {
+                $custom_access_groups.slideUp();
+                $custom_access_group_members.hide();
+            }
+
+            if ( ! init ) {
+                $this.data("lastselected",$this.val())
+            }
+        }).triggerHandler("change", rememberInitialValue);
+
+        function adjustSecurityDropdown(data) {
+            if ( ! data ) return;
+
+            var $security = $("#js-security");
+            var oldval = $security.data("lastselected");
+            var rank = { "public": "0", "access": "1", "private": "2", "custom": "3" };
+
+            $security.empty();
+            if ( data.ret ) {
+                if ( data.ret["minsecurity"] == "friends" ) data.ret["minsecurity"] = "access";
+
+                var opts;
+                if ( data.ret['is_comm'] ) {
+                    opts = [
+                        "<option value='public'>Everyone (Public)</option>",
+                        "<option value='access'>Members</option>"
+                    ];
+                    if ( data.ret['can_manage'] )
+                        opts.push("<option value='private'>Admin</option>");
+                } else {
+                    opts = [
+                        "<option value='public'>Everyone (Public)</option>",
+                        "<option value='access'>Access List</option>",
+                        "<option value='private'>Private (Just You)</option>"
+                    ];
+                    if ( data.ret['friend_groups_exist'] )
+                        opts.push("<option value='custom'>Custom</option>");
+                }
+
+                $security.append(opts.join("\n"))
+
+                // select the minsecurity value and disable the values with lesser security
+                $security.val(rank[oldval] >= rank[data.ret['minsecurity']] ? oldval : data.ret['minsecurity']);
+                if ( data.ret['minsecurity'] == 'access' ) {
+                    $security.find("option[value='public']").prop("disabled", true);
+                } else if ( data.ret['minsecurity'] == 'private' ) {
+                    $security.find("option[value='public'],option[value='access'],option[value='custom']")
+                        .prop("disabled", true);
+                }
+            } else {
+                // user is not known. no custom groups, no minsecurity
+                $security.append([
+                    "<option value='public'>Everyone (Public)</option>",
+                    "<option value='access'>Access List</option>",
+                    "<option value='private'>Private (Just You)</option>"
+                ].join("\n"))
+                $security.val(oldval);
+            }
+        }
+
+        $form.bind( "journalselect", function(e, journal) {
+            var anon = ! journal.name
+            if ( anon || journal.iscomm || ! journal.isremote )
+                $custom_access_groups.slideUp();
+
+            var $security = $("#js-security");
+            if ( $security.length > 0 ) {
+              if ( anon ) {
+                // no custom groups
+                adjustSecurityDropdown({})
+              } else if ( ! formData.edit ) {
+                $.getJSON( Site.siteroot + "/__rpc_getsecurityoptions",
+                    { "user": journal.name }, adjustSecurityDropdown);
+            }
+            $(this).data("journal",journal.name);
+          }
+        });
+
+        $custom_access_groups.find("input[name=custom_bit]").click(function(e) {
+            var members_data = []
+            var requests = []
+            $(this).parent().parent().find(":checkbox").each(function() {
+                if (this.checked) {
+                    requests.push($.getJSON("/__rpc_general?mode=list_filter_members&user=" + $form.data("journal") + "&filterid=" + this.value, function(data) {
+                        for ( member in data.filter_members.filterusers) {
+                            var the_name = data.filter_members.filterusers[member].fancy_username;
+                            var position = members_data.indexOf(the_name);
+                            if( position == -1) {
+                                members_data.push(the_name);
+                            }
+                        }
+                    }))
+                }
+            });
+
+            $.when.apply($, requests).done(function() {
+                var members_data_list = "";
+                members_data.sort();
+                for (member in members_data) {
+                    members_data_list = members_data_list + "<li>" + members_data[member] + "</li>";
+                }
+                $custom_access_group_members
+                    .slideDown()
+                    .find("ul")
+                        .html(members_data_list);
+            });
+        });
+    }
+
     var init = function(formData) {
         $("#nojs").val(0);
 
@@ -73,7 +192,8 @@ var postForm = (function($) {
 
         initCollapsible(entryForm);
 
-        initCurrents(formData.moodpics);
+        initCurrents(entryForm, formData.moodpics);
+        initAccess(entryForm, formData);
     };
 
     return {
