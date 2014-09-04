6b934438020be6033741c4636a1d2fab9d137a40
Afuna <fu@dreamwidth.org>
2014-09-04 18:15:07 +0800

    [#665] Currents component

--- htdocs/js/pages/entry/new.js
+++ htdocs/js/pages/entry/new.js
@@ -1,13 +1,78 @@
 var postForm = (function($) {
-    var initializeCollapsible = function() {
+    var initCollapsible = function() {
         $("#post_entry").collapse({ endpointUrl: "/__rpc_entryformcollapse" });
+    };
+
+    var initCurrents = function(moodpics) {
+        var $moodSelect = $("#js-current-mood");
+        var $customMood = $("#js-current-mood-other");
+
+        function _mood() {
+            var selectedMood = $moodSelect.val();
+            return moodpics[selectedMood] ? moodpics[selectedMood] : ["", ""];
+        }
+
+        var updatePreview = function() {
+            if ( ! moodpics ) return;
+
+            $("#js-moodpreview .moodpreview-image").fadeOut("fast", function() {
+                var $this = $(this);
+                $this.empty();
+
+                $("#js-moodpreview")
+                    .removeClass("columns medium-4")
+                    .prev()
+                        .removeClass("medium-8");
+
+                var mood = _mood();
+                if ( mood[1] !== "" ) {
+                    $this.append($("<img />",{ src: mood[1], width: mood[2], height: mood[3]}))
+                         .fadeIn();
+
+                    $("#js-moodpreview")
+                        .addClass("columns medium-4")
+                        .prev()
+                            .addClass("columns medium-8");
+                }
+            });
+        }
+
+        var updatePreviewText = function () {
+            var customMoodText = $customMood.val();
+            if ( ! customMoodText ) {
+                var mood = _mood();
+                customMoodText = mood[0];
+            }
+            $("#js-moodpreview .moodpreview-text").text( customMoodText );
+
+        }
+
+        // initialize...
+        if( moodpics ) {
+            $moodSelect
+                .change(updatePreview)
+                .change(updatePreviewText)
+                .closest(".columns")
+                    .after("<div id='js-moodpreview'>"
+                        + "<div class='moodpreview-text'></div>"
+                        + "<div class='moodpreview-image'></div>"
+                        + "</div>");
+
+            $customMood.change(updatePreviewText);
+
+            updatePreview();
+            updatePreviewText();
+        }
     };
 
     var init = function(formData) {
         $("#nojs").val(0);
-        initializeCollapsible();
 
         if ( ! formData ) formData = {};
+        initCollapsible();
+
+        initCurrents(formData.moodpics);
+
     };
 
     return {
