3d350ade1cab3c71c21337816b10f78788d48088
Afuna <fu@dreamwidth.org>
2014-11-07 10:43:03 -0800

    [#1044] Convert admin/healthy

--- cgi-bin/DW/Controller/Admin/StatusCheck.pm
+++ cgi-bin/DW/Controller/Admin/StatusCheck.pm
@@ -17,6 +17,7 @@ use DW::Controller;
 use DW::Routing;
 use DW::Template;
 use DW::Controller::Admin;
+use IO::Socket::INET ();
 
 =head1 NAME
 
@@ -24,12 +25,145 @@ DW::Controller::Admin::StatusCheck - Checks the status of various services
 
 =cut
 
+DW::Routing->register_string( "/admin/healthy", \&healthy_handler, format => 'plain' );
+DW::Controller::Admin->register_admin_page( '/',
+    path => 'healthy',
+    ml_scope => '/admin/index.tt',
+    link_ml => '.admin.healthy.link',
+    description_ml => '.admin.healthy.text'
+);
+
 DW::Routing->register_string( "/admin/theschwartz", \&theschwartz_handler );
 DW::Controller::Admin->register_admin_page( '/',
     path => 'theschwartz',
     ml_scope => '/admin/theschwartz.tt',
     privs => [ 'siteadmin:theschwartz' ]
 );
+
+# Returns some healthy-or-not statistics on the site.  Intended to be used by
+# remote monitoring services and the like.  This is supposed to be very
+# lightweight, not designed to replace Nagios monitoring in any way.
+# Printing as plain text to avoid having to parse HTML cruft
+sub healthy_handler {
+    my ( $opts ) = @_;
+
+    my ( $ok, $rv ) = controller( anonymous => 1 );
+    return $rv unless $ok;
+
+    my $r = $rv->{r};
+
+    my ( @pass, @fail );
+
+    # check 1) verify databases reachable
+    my $dbh = LJ::get_db_writer();
+    if ( $dbh ) {
+        my $time = $dbh->selectrow_array( 'SELECT UNIX_TIMESTAMP()' );
+        if ( ! $time || $dbh->err ) {
+            push @fail, "global writer test query failed";
+        } else {
+            push @pass, "global writer";
+        }
+    } else {
+        push @fail, "global writer unreachable";
+    }
+
+    # step 2) check all clusters
+    foreach my $cid ( @LJ::CLUSTERS ) {
+        my $dbcm = LJ::get_cluster_master( $cid );
+        if ( $dbcm ) {
+            my $time = $dbcm->selectrow_array( 'SELECT UNIX_TIMESTAMP()' );
+            if ( ! $time || $dbcm->err ) {
+                push @fail, "cluster $cid writer test query failed";
+            } else {
+                push @pass, "cluster $cid writer";
+            }
+        } else {
+            push @fail, "cluster $cid writer unreachable";
+        }
+    }
+
+    # verify connectivity to all memcache machines
+    foreach my $memc ( @LJ::MEMCACHE_SERVERS ) {
+        my $sock = IO::Socket::INET->new( PeerAddr => $memc, Timeout => 1 );
+
+        if ( $sock ) {
+            push @pass, "memcache $memc";
+        } else {
+            push @fail, "memcache $memc";
+        }
+    }
+
+    # check each mogilefs server
+    foreach my $mog ( @{ $LJ::MOGILEFS_CONFIG{hosts} || [] } ) {
+        my $sock = IO::Socket::INET->new( PeerAddr => $mog, Timeout => 1 );
+
+        if ( $sock ) {
+            push @pass, "mogilefsd $mog";
+        } else {
+            push @fail, "mogilefsd $mog";
+        }
+    }
+
+    # check each gearman server
+    foreach my $gm ( @LJ::GEARMAN_SERVERS ) {
+        my $sock = IO::Socket::INET->new( PeerAddr => $gm, Timeout => 1 );
+
+        if ( $sock ) {
+            push @pass, "gearman $gm";
+        } else {
+            push @fail, "gearman $gm";
+        }
+    }
+
+    # and each Perlbal
+    foreach my $pb ( values %LJ::PERLBAL_SERVERS ) {
+        my $sock = IO::Socket::INET->new( PeerAddr => $pb, Timeout => 1 );
+
+        if ( $sock ) {
+            push @pass, "perlbal $pb";
+        } else {
+            push @fail, "perlbal $pb";
+        }
+    }
+
+    if ( ! LJ::theschwartz() ) {
+        # no schwartz
+    } elsif ( scalar( grep { defined $_->{role} } @LJ::THESCHWARTZ_DBS ) > 0 || scalar( @LJ::THESCHWARTZ_DBS ) > 1 ) {
+        # cannot test, leaving off
+    } else {
+        my $sid = 0;
+        foreach my $db ( @LJ::THESCHWARTZ_DBS ) {
+            my $s_db = DBI->connect( $db->{dsn}, $db->{user}, $db->{pass} );
+            if ( $s_db ) {
+                my $time = $s_db->selectrow_array( "DESCRIBE " . ( $db->{prefix} ? $db->{prefix}."_job" : "job" ) );
+                if ( ! $time || $s_db->err ) {
+                    push @fail, "schwartz $sid";
+                } else {
+                    push @pass, "schwartz $sid";
+                }
+            } else {
+                push @fail, "schwartz $sid unreachable";
+            }
+            $sid++;
+        }
+    }
+
+    my $out = '';
+    if ( @fail ) {
+        $out = "status=fail\n\nfailures:\n";
+        $out .= join( "\n", map { "  $_" } @fail ) . "\n";
+    } else {
+        $out = "status=ok\n";
+    }
+
+    if ( @pass ) {
+        $out .= "\nokay:\n";
+        $out .= join( "\n", map { "  $_" } @pass ) . "\n";
+    }
+
+    $r->print( $out );
+    return $r->OK;
+}
 
 sub theschwartz_handler {
     my ( $opts ) = @_;
