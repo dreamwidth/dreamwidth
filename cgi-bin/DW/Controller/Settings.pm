aeea667dd2acf8706a445a98448d42750d4ae452
Afuna <fu@dreamwidth.org>
2014-11-11 13:46:26 -0800

    [#1058] Convert /changepassword

--- cgi-bin/DW/Controller/Settings.pm
+++ cgi-bin/DW/Controller/Settings.pm
@@ -35,6 +35,8 @@ DW::Controller::Settings - Controller for settings/settings-related pages
 =cut
 
 DW::Routing->register_string( "/accountstatus", \&account_status_handler, app => 1 );
+DW::Routing->register_string( "/changepassword", \&changepassword_handler, app => 1, prefer_ssl => 1 );
+
 
 sub account_status_handler {
     my ( $opts ) = @_;
@@ -201,4 +203,141 @@ sub account_status_handler {
     return DW::Template->render_template( 'settings/accountstatus.tt', $vars );
 }
 
+sub changepassword_handler {
+    my ( $opts ) = @_;
+
+    my ( $ok, $rv ) = controller( anonymous => 1 );
+    return $rv unless $ok;
+
+    my $r = $rv->{r};
+    my $get = $r->get_args;
+    my $post;
+
+    my $remote = $rv->{remote};
+
+    my ( $aa, $authu );
+    my $ml_scope = "/settings/changepassword.tt";
+    if ( my $auth = $get->{auth} ) {
+        my $lostinfo_url = LJ::create_url( "/lostinfo" );
+
+        return error_ml( "$ml_scope.error.invalidarg" )
+            unless $auth =~ /^(\d+)\.(.+)$/;
+
+        $aa = LJ::is_valid_authaction( $1, $2 );
+        return error_ml( "$ml_scope.error.invalidarg" )
+            unless $aa;
+
+        return error_ml( "$ml_scope.error.actionalreadyperformed", { url => $lostinfo_url } )
+            if $aa->{used} eq 'Y';
+
+        return $r->redirect( $lostinfo_url )
+             unless $aa->{action} eq 'reset_password';
+
+         # confirmed the identity...
+         $authu = LJ::load_userid( $aa->{userid} );
+
+         # verify the email can still receive passwords
+         return error_ml( "$ml_scope.error.emailchanged", { url => $lostinfo_url } )
+             unless $authu->can_receive_password( $aa->{arg1} );
+    }
+    return error_ml( "$ml_scope.error.identity" ) if $remote && $remote->is_identity;
+
+    my $errors = DW::FormErrors->new;
+    if ( $r->did_post && $r->post_args->{mode} eq 'submit' ) {
+        $post = $r->post_args;
+
+        my $user = $authu ? $authu->user : LJ::canonical_username( $post->{user} );
+        my $password = $post->{password};
+        my $newpass1 = LJ::trim( $post->{newpass1} );
+        my $newpass2 = LJ::trim( $post->{newpass2} );
+
+        my $u = LJ::load_user( $user );
+        $errors->add( "user", ".error.invaliduser" ) unless $u;
+        $errors->add( "user", ".error.identity" ) if $u && $u->is_identity;
+        $errors->add( "user", ".error.changetestaccount" ) if grep { $user eq $_ } @LJ::TESTACCTS;
+
+        unless ( $errors->exist ) {
+            if ( LJ::login_ip_banned( $u ) ) {
+                $errors->add( "user", "error.ipbanned" );
+            } elsif ( ! $authu && ( $u->password eq "" || $u->password ne $password ) ) {
+                $errors->add( "password", ".error.badoldpassword" );
+                LJ::handle_bad_login( $u );
+            }
+        }
+
+        if ( ! $newpass1 ) {
+            $errors->add( "newpass1", ".error.blankpassword" );
+        } elsif ( $newpass1 ne $newpass2 ) {
+            $errors->add( "newpass2", ".error.badnewpassword" );
+        } else {
+            my $checkpass = LJ::CreatePage->verify_password( password => $newpass1, u => $u );
+            $errors->add( "newpass1", ".error.badcheck", { error => $checkpass } )
+                if $checkpass;
+        }
+
+        # don't allow changes if email address is not validated, unless they
+        # have a bad password or got the reset email
+        $errors->add( "newpass1", ".error.notvalidated" )
+            if $u->{status} ne 'A' && ! $u->prop( 'badpassword' ) && ! $authu;
+
+        # now let's change the password
+        unless ( $errors->exist ) {
+            ## make note of changed password
+            my $dbh = LJ::get_db_writer();
+            my $oldval = Digest::MD5::md5_hex( $u->password . "change" );
+            $u->infohistory_add( 'password', $oldval );
+
+            $u->log_event('password_change', { remote => $remote } );
+
+            $u->update_self( { password => $post->{newpass1} } );
+
+            # if we used an authcode, we'll need to expire it now
+            LJ::mark_authaction_used( $aa ) if $authu;
+
+            # If we forced them to change their password, mark them as now being good
+            $u->set_prop( 'badpassword', 0 ) if LJ::is_enabled( 'force_pass_change' );
+
+            # Kill all sessions, forcing user to relogin
+            $u->kill_all_sessions;
+
+            LJ::send_mail( {
+                'to' => $u->email_raw,
+                'from' => $LJ::ADMIN_EMAIL,
+                'fromname' => $LJ::SITENAME,
+                'charset' => 'utf-8',
+                'subject' => LJ::Lang::ml( "$ml_scope.email.subject" ),
+                'body' => LJ::Lang::ml( "$ml_scope.email.body2",
+                                  { sitename => $LJ::SITENAME,
+                                    siteroot => $LJ::SITEROOT,
+                                    username => $u->{user},
+                                  } ),
+            } );
+
+            my $success_ml = $remote ? "settings/changepassword.tt.withremote" : "settings/changepassword.tt";
+            return DW::Controller->render_success( $success_ml,  {
+                    url => LJ::create_url( "/login" ),
+                } );
+
+            LJ::Hooks::run_hooks( "post_changepassword", {
+                "u" => $u,
+                "newpassword" => $post->{newpass1},
+                "oldpassword" => $u->password,
+            } );
+
+            LJ::Hooks::run_hook( 'user_login', $u );
+        }
+    }
+
+    my $vars = {
+        bad_password        => $remote && $remote->prop( 'badpassword' ),
+        needs_validation    => ! $authu && $remote && ! $remote->prop( 'badpassword' ) && ! $r->did_post && $remote->{status} ne 'A',
+
+        authu   => $authu,
+        remote  => $remote,
+
+        formdata => $post || { user => $remote ? $remote->user : "" },
+        errors   => $errors,
+    };
+    return DW::Template->render_template( 'settings/changepassword.tt', $vars );
+}
 1;
