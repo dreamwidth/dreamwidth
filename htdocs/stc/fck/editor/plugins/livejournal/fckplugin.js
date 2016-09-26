85c4935803d50e51eef70dfb11b7234987932a42
Kareila <kareila@dreamwidth.org>
2016-09-26 09:00:07 -0500

    [#1869] remove unused LJVideoCommand

--- htdocs/stc/fck/editor/plugins/livejournal/fckplugin.js
+++ htdocs/stc/fck/editor/plugins/livejournal/fckplugin.js
@@ -181,65 +181,6 @@ oLJUserLink.IconPath = FCKConfig.PluginsPath + 'livejournal/dwuser.png' ;
 // Register the button to use in the config
 FCKToolbarItems.RegisterItem('LJUserLink', oLJUserLink) ;
 
-
-//////////  LJ Video Button //////////////
-var LJVideoCommand=function(){
-};
-LJVideoCommand.prototype.Execute=function(){
-}
-LJVideoCommand.GetState=function() {
-    return FCK_TRISTATE_OFF; //we dont want the button to be toggled
-}
-
-LJVideoCommand.Execute=function() {
-    var url;
-    var selection = '';
-
-    if (FCK.EditorWindow.getSelection) {
-        selection = FCK.EditorWindow.getSelection();
-        // Create a new div to clone the selection's content into
-        var d = FCK.EditorDocument.createElement('DIV');
-        for (var i = 0; i < selection.rangeCount; i++) {
-            d.appendChild(selection.getRangeAt(i).cloneContents());
-        }
-        selection = d.innerHTML;
-    } else if (FCK.EditorDocument.selection) {
-        var range = FCK.EditorDocument.selection.createRange();
-        var type = FCKSelection.GetType();
-        if (type == 'Control') {
-            selection = range.item(0).outerHTML;
-        } else if (type == 'None') {
-            selection = '';
-        } else {
-            selection = range.htmlText;
-        }
-    }
-
-    if (selection != '') {
-        url = selection;
-    } else {
-        url = prompt(window.parent.FCKLang.VideoPrompt,'');
-    }
-
-    if (url != null && url != '') {
-        // Make the tag like the editor would
-        var html = "<div url=\""+url+"\" class=\"ljvideo\"><img src=\""+FCKConfig.PluginsPath + "livejournal/ljvideo.gif\" /></div>";
-
-        FCK.InsertHtml(html);
-        FCKSelection.Collapse();
-        FCK.Focus();
-    }
-    return;
-}
-
-FCKCommands.RegisterCommand('LJVideoLink', LJVideoCommand); //otherwise our command will not be found
-
-// Create the toolbar button.
-var oLJVideoLink = new FCKToolbarButton('LJVideoLink', window.parent.FCKLang.LJVideo);
-oLJVideoLink.IconPath = FCKConfig.PluginsPath + 'livejournal/ljvideo.gif';
-
-// Register the button to use in the config
-FCKToolbarItems.RegisterItem('LJVideoLink', oLJVideoLink);
 //////////  LJ Embed Media Button //////////////
 var LJEmbedCommand=function(){};
 LJEmbedCommand.prototype.Execute=function(){};
