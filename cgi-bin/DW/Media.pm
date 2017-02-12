ef8d8017fc90c0ec2b34cba1d0787fb594385389
Kareila <kareila@dreamwidth.org>
2017-02-11 23:58:09 -0600

    incorporate feedback from code review

--- cgi-bin/DW/Media.pm
+++ cgi-bin/DW/Media.pm
@@ -229,7 +229,7 @@ sub get_usage_for_user {
     my ( $usage ) = $u->selectrow_array(
         q{SELECT SUM(mv.filesize) FROM media_versions AS mv, media AS m
           WHERE mv.userid=? AND m.userid=mv.userid AND m.mediaid=mv.mediaid
-          AND m.state = 'A';
+          AND m.state = 'A'
          },
         undef, $u->id
     );
