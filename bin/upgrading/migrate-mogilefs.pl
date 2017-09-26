bf49bfd944e16192f30c4119c476609eab8444df
Mark Smith <mark@dreamwidth.org>
2017-09-26 05:45:16 +0000

    Update migrator

--- bin/upgrading/migrate-mogilefs.pl
+++ bin/upgrading/migrate-mogilefs.pl
@@ -26,72 +26,121 @@ use MogileFS::Client;
 
 use DW::BlobStore;
 
-my ( $nextfid, $endfid, $conf );
+use constant BLOCK_SIZE => 1_000;
+
+my ( $startfid, $endfid, $max_workers, $conf );
 GetOptions(
-    'start-fid=i'       => \$nextfid,
+    'start-fid=i'       => \$startfid,
     'end-fid=i'         => \$endfid,
+    'num-workers=i'     => \$max_workers,
     'mogilefs-config=s' => \$conf,
 );
-$nextfid ||= 0;
+$startfid ||= 0;
 $endfid ||= 1_000_000_000;
+$max_workers ||= 10;
 
 die "Must provide valid --mogilefs-config=FILEPATH argument.\n"
     unless $conf && -e $conf;
 
-my $dbh = get_mogilefs_dbh();
 my $mogc = get_mogilefs_client();
 
-# Main loop; just get some files and do cool things with them. In essence we're just getting
-# them from MogileFS and storing them to BlobStore.
-while ( 1 ) {
-    my $files = get_some_files( $dbh, $nextfid+1, $endfid );
-    if ( ! defined $files ) {
-        say "Looks like we're done, good-bye!";
-        exit;
-    }
+my $queue = [];
+my $cur_workers = 0;
+my $num_workers = 0;
+my $enqueued = 0;
+while ( $startfid <= $endfid ) {
+    my $files = get_files( $startfid, $startfid + ( BLOCK_SIZE - 1 ) );
+    $startfid += BLOCK_SIZE;
+    next unless $files;
 
     foreach my $fid ( keys %$files ) {
-        $nextfid = $fid if $fid > $nextfid;
+        $enqueued++;
+        push @$queue, $files->{$fid};
+        next if scalar @$queue < BLOCK_SIZE;
+
+        $0 = sprintf( 'migrate-mogilefs: enqueued %d, workers %d',
+                $enqueued, $num_workers );
+
+        if ( $cur_workers >= $max_workers ) {
+            wait;
+            $cur_workers--;
+        }
+
+        make_worker( $queue );
+
+        $num_workers++;
+        $cur_workers++;
+        $queue = [];
+    }
+}
+
+if ( $queue && @$queue ) {
+    make_worker( $queue );
+    $cur_workers++;
+}
+
+while ( $cur_workers > 0 ) {
+    wait;
+    $cur_workers--;
+}
+say "All done.";
+
+sub make_worker {
+    my $queue = $_[0];
+
+    if ( my $pid = fork ) {
+        return;
+    }
+
+    my $pos = 0;
+    my $llen = scalar @$queue;
+    foreach my $file ( @$queue ) {
+        $pos++;
+        $0 = sprintf( 'migrate-mogilefs [%d/%d] = %0.2f%%',
+            $pos, $llen, 100*($pos/$llen) );
 
         # Quick check to make sure this file isn't already in BlobStore
-        if ( DW::BlobStore->exists( $files->{$fid}->{class} => $files->{$fid}->{key} ) ) {
-            say "$fid: OK EXISTS";
+        if ( DW::BlobStore->exists( $file->{class} => $file->{key} ) ) {
+            say "$file->{fid}: OK EXISTS";
             next;
         }
 
-        my $data = $mogc->get_file_data( $files->{$fid}->{key} );
+        my $data = $mogc->get_file_data( $file->{key} );
         unless ( $data ) {
-            say "$fid: ERR NODATA";
+            say "$file->{fid}: ERR NODATA";
             next;
         }
 
         my $size = length $$data;
-        unless ( $size == $files->{$fid}->{size} ) {
-            say "$fid: ERR WRONGSIZE $size $files->{$fid}->{size}";
+        unless ( $size == $file->{size} ) {
+            say "$file->{fid}: ERR WRONGSIZE $size $file->{size}";
             next;
         }
 
         # It's a file and the length is write, let's store it to BlobStore with the
         # right data...
-        my $rv = DW::BlobStore->store( $files->{$fid}->{class} => $files->{$fid}->{key}, $data );
+        my $rv = DW::BlobStore->store( $file->{class} => $file->{key}, $data );
         if ( $rv ) {
-            say "$fid: OK STORE";
+            say "$file->{fid}: OK STORE";
         } else {
-            say "$fid: ERR STORE";
+            say "$file->{fid}: ERR STORE";
         }
     }
+
+    exit;
 }
 
-sub get_some_files {
-    my ( $dbh, $start_fid, $end_fid ) = @_;
+sub get_files {
+    my ( $start_fid, $end_fid ) = @_;
+
+    my $dbh = get_mogilefs_dbh();
 
     my $rows = $dbh->selectall_arrayref(
         q{SELECT f.fid, f.dkey, f.length, d.namespace, c.classname
           FROM file f, domain d, class c
-          WHERE f.fid >= ? AND f.fid < ? AND
+          WHERE f.fid >= ? AND f.fid <= ? AND
                 d.dmid = f.dmid AND c.dmid = f.dmid AND c.classid = f.classid
-          ORDER BY f.fid
-          LIMIT 100},
+        },
         undef, $start_fid, $end_fid,
     );
     return undef unless $rows && scalar @$rows;
@@ -99,6 +148,7 @@ sub get_some_files {
     my $out = {};
     foreach my $row ( @$rows ) {
         $out->{$row->[0]} = {
+            fid    => $row->[0],
             key    => $row->[1],
             size   => $row->[2],
             domain => $row->[3],
@@ -127,11 +177,11 @@ sub get_mogilefs_dbh {
 
 sub get_mogilefs_client {
     my $mogclient = MogileFS::Client->new(
-		domain   => $LJ::MOGILEFS_CONFIG{domain},
-		root     => $LJ::MOGILEFS_CONFIG{root},
-		hosts    => $LJ::MOGILEFS_CONFIG{hosts},
-		timeout  => $LJ::MOGILEFS_CONFIG{timeout},
-	)
+       domain   => $LJ::MOGILEFS_CONFIG{domain},
+       root     => $LJ::MOGILEFS_CONFIG{root},
+       hosts    => $LJ::MOGILEFS_CONFIG{hosts},
+       timeout  => $LJ::MOGILEFS_CONFIG{timeout},
+   )
         or die "Failed to create MogileFS::Client!\n";
     return $mogclient;
 }
