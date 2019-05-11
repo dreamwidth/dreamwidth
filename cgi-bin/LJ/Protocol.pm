dd51c367c68efeacf11f470fef3608c60b522c35
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-11 18:05:06 +0100

    Remove an unused variable in Protocol.pm
    Per the issue notes, there's no limit on community moderators
    anywhere else and this `last` statement will never execute, so just
    remove it.

--- cgi-bin/LJ/Protocol.pm
+++ cgi-bin/LJ/Protocol.pm
@@ -1491,10 +1491,7 @@ sub postevent
                 my $modlist = LJ::load_userids(@$mods);
 
                 my @emails;
-                my $ct;
                 foreach my $mod (values %$modlist) {
-                    last if $ct > 20;  # don't send more than 20 emails.
-
                     next unless $mod->is_visible;
 
                     LJ::Event::CommunityModeratedEntryNew->new( $mod, $uowner, $modid )->fire;
