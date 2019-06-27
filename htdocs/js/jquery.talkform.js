1c0214881db4ba52131aac343c29a7ec9fc95810
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-06-26 23:53:41 -0700

    Rewrite talkform javascript
    - Deduplicate some shared reply form logic into jquery.replyforms.js.
        - Add the max comment length constant to the Site object.
        - Change how the quote button is added -- now it's a real button in the
          markup that gets unhidden, instead of a span that gets a button appended
          to it.
    - Change some quickreply JS logic.
        - Move custom form submit logic into a submit handler instead of button
          click handlers.
        - Fix bug where QR from reading page wouldn't let you proceed if there was
          a captcha. Now it goes to talkform, like QR from comment page does.
    - Replace finely minced talkform JS with a small amount of jQuery.
        - Use constant for max username length.
        - Stop doing the MD5 password hash thing to avoid sending password over the
          wire, since it goes over TLS now.
        - Delete globally inlined function to do same (but don't worry, it's only
          one of at least three functionally identical implementations).
        - Instead of complaining about incoherent combination of form fields, just
          prevent it.
        - Match behavior of "From" radio buttons to apparent intended behavior:
            - Instead of magic "`[other]`" link, just always show the other site
              user option.
            - When unselecting an option that has associated auth fields (other
              openid user, other site user), hide those fields and also reset them
              to their initial values. (Old JS did the one but not the other.)
    - Simplify markup of "From" radio buttons in talkform.
        - Use disabled radio inputs instead of ascii art `()`.
        - Instead of jumping through outrageous hoops to split the label in half and
          put the radio button in the middle, just include the image in the label.
        - Basic divs instead of unnecessary sub-table of unnecessary table.

---
+++ htdocs/js/jquery.talkform.js
@@ -0,0 +1,88 @@
+// Helpers specific to the talkform aka slowreply aka ReplyPage aka The Varmint
+// See also jquery.quickreply.js, jquery.replyforms.js
+jQuery(function($){
+    var commentForm = $('#postform');
+    var fromOptions = $('input[name="usertype"]');
+    var authForms = $('.from-login');
+    var iconSelect = $('#prop_picture_keyword');
+
+    // Reset any input children to their initial values.
+    jQuery.fn.extend({
+        resetFormFields: function() {
+            this.find('input').each(function(i, elm){
+                var type = elm.getAttribute('type');
+                if (type === 'checkbox' || type === 'radio') {
+                    elm.checked = elm.defaultChecked;
+                } else {
+                    elm.value = elm.defaultValue;
+                }
+            });
+            return this;
+        }
+    });
+
+    // Tidy up irrelevant controls when choosing who the comment is from
+    fromOptions.change(function(e) {
+        // When a "from" option is selected, show its associated login form (if
+        // any), and reset the values of any other login forms.
+        var associatedLoginForm = document.getElementById( $(this).data('more') );
+        authForms.hide();
+        $(associatedLoginForm).show();
+        authForms.not(associatedLoginForm).resetFormFields();
+
+        // Icon select menu is only available for logged-in user
+        if (this.id === 'talkpostfromremote' || this.id === 'talkpostfromoidli') {
+            iconSelect.prop('disabled', false);
+        } else {
+            iconSelect.val('');
+            iconSelect.prop('disabled', true);
+        }
+    });
+
+    // setup: hide login forms. show the currently relevant one, if any.
+    fromOptions.filter(':checked').change();
+
+    // Clear username when clicked (but not focused, so we don't clear it as a
+    // keyboard or reader user navigates through it)
+    $('#postform #username').click(function(e){
+        this.value = '';
+    });
+
+    // subjecticons :|
+    $('#subjectIconImage').click(function(){
+        $('#subjectIconList').toggle();
+    });
+    $('#subjectIconList').find('img').click(function(){
+        $('#subjectIconField').val(this.id);
+        $('#subjectIconImage')
+            .attr('src', this.src)
+            .attr('width', this.width)
+            .attr('height', this.height);
+        $('#subjectIconList').hide();
+    });
+
+    // nohtml messages :|
+    $('#subject').keyup(function(e) {
+        if (this.value.includes('<')) {
+            $('#ljnohtmlsubj').show();
+        }
+    });
+    $('#editreason').keyup(function(e) {
+        if (this.value.includes('<')) {
+            $('#nohtmledit').show();
+        }
+    });
+
+    // submit handlers :|
+    commentForm.submit(function(e) {
+        // idek what would blow up if you sent name/pwd when usertype is
+        // something other than user, but the old js did this so whatever
+        if ( ! $('#talkpostfromlj').prop('checked') ) {
+            $('#username').val('');
+            $('#password').val('');
+        }
+        // prevent double-submits
+        $form.find('input[type="submit"]').prop("disabled", true);
+    });
+
+});
