f9ef2b6997aaae0911efe70cf2810791cb30e307
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-07-07 22:50:22 -0700

    [#2427] [#2570] Use Foundation in S2 pages
    This is gated on a beta feature called `s2foundation`.
    
    In site scheme entry/reply/icons, this changes all of the styling.
    
    In journal-styled pages this doesn't change any visible styling, but it DOES
    enable the use of Foundation-based components in those pages. It uses a new
    minimal CSS file to set up Foundation's prerequisites without changing anything
    important. (Foundation checks for metadata hidden in weird dummy style rules
    before it'll let its JavaScript do anything, and also it relies on Modernizr
    classes.)
    
    The beta also enables need_res within components called by journal-styled S2
    pages... but only for JavaScript includes, not CSS includes. It's an issue with
    S2 eagerly printing stuff, alas.
    
    This commit also adds a new read-only variable to S2 Page objects that indicates
    whether the control strip will be shown, because that was the lowest-impact way
    to deal with the control strip JS.
    
    Known issues in the beta as of this commit:
    
    - A bunch of JavaScript in journal pages is broken, because it's not being added
      to the "foundation" resource group.
    - Site skin entry/reply/icons pages look bad because they don't have the
      appropriate CSS yet.

--- etc/config-local.pl
+++ etc/config-local.pl
@@ -160,10 +160,9 @@
 #            start_time => 0,
 #            end_time => "Inf",
 #        },
-#        "s2comments" => {
+#        "s2foundation" => {
 #            start_time => 0,
 #            end_time => "Inf",
-#            sitewide => 1,
 #        },
 #    );
 
