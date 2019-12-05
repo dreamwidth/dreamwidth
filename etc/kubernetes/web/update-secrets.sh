5cac6d35b9a9073ddbaa05948d511a86b0e5dc36
Mark Smith <mark@discordapp.com>
2019-12-04 18:57:15 -0800

    Don't use secrets folder, easier to use

--- etc/kubernetes/web/update-secrets.sh
+++ etc/kubernetes/web/update-secrets.sh
@@ -4,10 +4,10 @@
 #
 
 kubectl create secret generic dw-config \
-        --from-file=config.pl=secrets/config.pl \
-        --from-file=config-local.pl=secrets/config-local.pl \
-        --from-file=config-private.pl=secrets/config-private.pl \
-        --from-file=log4perl.conf=secrets/log4perl.conf \
-        --from-file=config-private-prod.pl=secrets/config-private-prod.pl \
+        --from-file=config.pl=config.pl \
+        --from-file=config-local.pl=config-local.pl \
+        --from-file=config-private.pl=config-private.pl \
+        --from-file=log4perl.conf=log4perl.conf \
+        --from-file=config-private-prod.pl=config-private-prod.pl \
         --dry-run -o yaml |
     kubectl apply -f -
