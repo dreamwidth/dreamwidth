5ec20ad062a0eb7b9e4d5912042f6c5456dce16f
Ruth <momijizukamori@gmail.com>
2020-05-07 22:46:03 +0000

    convert site/opensource

--- cgi-bin/DW/Controller/Misc.pm
+++ cgi-bin/DW/Controller/Misc.pm
@@ -38,6 +38,7 @@ DW::Routing->register_string( "/community/random/index",  \&random_community_han
 DW::Routing->register_string( "/beta",                    \&beta_handler,             app  => 1 );
 
 DW::Routing->register_static( '/internal/404', 'error/404.tt', app => 1 );
+DW::Routing->register_static( '/site/opensource', 'site/opensource.tt', app => 1 );
 
 sub beta_handler {
     my ($opts) = @_;
