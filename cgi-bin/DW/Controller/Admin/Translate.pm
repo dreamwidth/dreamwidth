a676af9f719d03e9b9969cac9c54ca684fff9d3e
Kareila <kareila@dreamwidth.org>
2020-06-11 00:18:21 -0500

    convert /admin/translate/search and /admin/translate/searchform

--- cgi-bin/DW/Controller/Admin/Translate.pm
+++ cgi-bin/DW/Controller/Admin/Translate.pm
@@ -29,10 +29,12 @@ DW::Controller::Admin->register_admin_page(
     ml_scope => '/admin/translate/index.tt',
 );
 
-DW::Routing->register_string( "/admin/translate/index",         \&index_controller,    app => 1 );
-DW::Routing->register_string( "/admin/translate/edit",          \&edit_controller,     app => 1 );
-DW::Routing->register_string( "/admin/translate/help-severity", \&severity_controller, app => 1 );
-DW::Routing->register_string( "/admin/translate/welcome",       \&welcome_controller,  app => 1 );
+DW::Routing->register_string( "/admin/translate/index",         \&index_controller,      app => 1 );
+DW::Routing->register_string( "/admin/translate/edit",          \&edit_controller,       app => 1 );
+DW::Routing->register_string( "/admin/translate/search",        \&search_controller,     app => 1 );
+DW::Routing->register_string( "/admin/translate/searchform",    \&searchform_controller, app => 1 );
+DW::Routing->register_string( "/admin/translate/help-severity", \&severity_controller,   app => 1 );
+DW::Routing->register_string( "/admin/translate/welcome",       \&welcome_controller,    app => 1 );
 
 sub index_controller {
     my ( $ok, $rv ) = controller();
@@ -108,6 +110,125 @@ sub edit_controller {
 
     return DW::Template->render_template( 'admin/translate/edit.tt', $form_args,
         { no_sitescheme => 1 } );
+}
+
+sub search_controller {
+    my ( $ok, $rv ) = controller();
+    return $rv unless $ok;
+
+    my $r         = $rv->{r};
+    my $form_args = $r->get_args;
+    my $vars      = { lang => $form_args->{lang} };
+
+    my $l = LJ::Lang::get_lang( $vars->{lang} );
+
+    my $err = sub { DW::Template->render_string( $_[0], { no_sitescheme => 1 } ) };
+
+    return $err->("<b>Invalid language</b>") unless $l;
+
+    my $dbr = LJ::get_db_reader();
+    my $sql;
+
+    # construct database query
+    {
+        # all queries in production use the visible flag
+        my $vis_flag = $LJ::IS_DEV_SERVER ? '' : 'AND i.visible = 1';
+
+        if ( $form_args->{search} eq 'sev' ) {
+            my $what = ">= 1";
+            if ( $form_args->{stale} =~ /^(\d+)(\+?)$/ ) {
+                $what = ( $2 ? ">=" : "=" ) . $1;
+            }
+            $sql = qq(
+                SELECT i.dmid, i.itid, i.itcode FROM ml_items i, ml_latest l
+                WHERE l.lnid=$l->{'lnid'} AND l.staleness $what AND l.dmid=i.dmid
+                AND l.itid=i.itid $vis_flag ORDER BY i.dmid, i.itcode
+            );
+        }
+
+        if ( $form_args->{search} eq 'txt' ) {
+            my $remote = $rv->{remote};
+            return $err->("This search type is restricted to $l->{'lnname'} translators.")
+                unless $remote
+                && ( $remote->has_priv( "translate", $l->{'lncode'} )
+                || $remote->has_priv( "faqedit", "*" ) );    # FAQ admins can search too
+
+            my $qtext     = $dbr->quote( $form_args->{searchtext} );
+            my $dmid      = $form_args->{searchdomain} + 0;
+            my $dmidwhere = $dmid ? "AND i.dmid=$dmid" : "";
+
+            if ( $form_args->{searchwhat} eq "code" ) {
+                $sql = qq{
+                    SELECT i.dmid, i.itid, i.itcode FROM ml_items i, ml_latest l
+                    WHERE l.lnid=$l->{'lnid'} AND l.dmid=i.dmid AND i.itid=l.itid $vis_flag
+                    $dmidwhere AND LOCATE($qtext, i.itcode)
+                };
+            }
+            else {
+                my $lnid = $l->{'lnid'};
+                $lnid = $l->{'parentlnid'} if $form_args->{searchwhat} eq "parent";
+
+                $sql = qq{
+                    SELECT i.dmid, i.itid, i.itcode FROM ml_items i, ml_latest l, ml_text t
+                    WHERE l.lnid=$lnid AND l.dmid=i.dmid AND i.itid=l.itid
+                    $dmidwhere AND t.dmid=l.dmid AND t.txtid=l.txtid
+                    AND LOCATE($qtext, t.text) $vis_flag ORDER BY i.itcode
+                };
+            }
+        }
+
+        if ( $form_args->{search} eq 'flg' ) {
+            return $err->("This type of search isn't available for this language.")
+                unless $l->{'lncode'} eq $LJ::DEFAULT_LANG || !$l->{'parentlnid'};
+
+            my $whereflags = join ' AND ',
+                map  { $form_args->{"searchflag$_"} eq 'yes' ? "$_ = 1" : "$_ = 0" }
+                grep { $form_args->{"searchflag$_"} ne 'whatev' } qw(proofed updated);
+            $whereflags = "AND $whereflags" if $whereflags ne '';
+
+            $sql = qq(
+                SELECT i.dmid, i.itid, i.itcode FROM ml_items i, ml_latest l
+                WHERE l.lnid=$l->{lnid} AND l.dmid=i.dmid AND l.itid=i.itid $whereflags $vis_flag
+                ORDER BY i.dmid, i.itcode
+            );
+        }
+
+        return $err->("Bogus or unimplemented query type.") unless $sql;
+    }
+
+    # each row contains 3 elements: (dmid, itid, itcode)
+    $vars->{rows} = $dbr->selectall_arrayref($sql);
+
+    # helper function for constructing links in template
+    $vars->{join} = sub {
+        my $pages = $_[0];
+        return LJ::eurl( join( ",", map { "$_->[0]:$_->[1]" } @$pages ) );
+    };
+
+    return DW::Template->render_template( 'admin/translate/search.tt',
+        $vars, { no_sitescheme => 1 } );
+}
+
+sub searchform_controller {
+    my ( $ok, $rv ) = controller();
+    return $rv unless $ok;
+
+    my $r         = $rv->{r};
+    my $form_args = $r->get_args;
+    my $vars      = { lang => $form_args->{lang} };
+
+    $vars->{l} = LJ::Lang::get_lang( $vars->{lang} );
+
+    return $r->redirect('/admin/translate/') unless $vars->{l};
+
+    $vars->{pl} = LJ::Lang::get_lang_id( $vars->{l}->{parentlnid} );
+
+    $vars->{domains} = [ sort { $a->{dmid} <=> $b->{dmid} } LJ::Lang::get_domains() ];
+
+    $vars->{def_lang} = $LJ::DEFAULT_LANG;
+
+    return DW::Template->render_template( 'admin/translate/searchform.tt',
+        $vars, { no_sitescheme => 1 } );
 }
 
 sub severity_controller {
