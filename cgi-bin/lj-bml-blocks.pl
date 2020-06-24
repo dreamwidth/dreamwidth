9c18db198fb0aac42d29253f1893e7256517f7cc
Kareila <kareila@dreamwidth.org>
2020-06-24 01:46:38 -0500

    BML blocks CHALRESPJS and METACTYPE defined but not used anywhere

--- cgi-bin/lj-bml-blocks.pl
+++ cgi-bin/lj-bml-blocks.pl
@@ -25,7 +25,6 @@ BML::register_block( "SITEROOT",      "S", $LJ::SITEROOT );
 BML::register_block( "SITENAME",      "S", $LJ::SITENAME );
 BML::register_block( "ADMIN_EMAIL",   "S", $LJ::ADMIN_EMAIL );
 BML::register_block( "SUPPORT_EMAIL", "S", $LJ::SUPPORT_EMAIL );
-BML::register_block( "CHALRESPJS",    "",  $LJ::COMMON_CODE{'chalresp_js'} );
 BML::register_block( "JSPREFIX",      "S", $LJ::JSPREFIX );
 
 # dynamic blocks to implement calling our ljuser function to generate HTML
@@ -57,8 +56,5 @@ BML::register_block(
     my $dl = "<a href=\"$LJ::SITEROOT/files/%%DATA%%\">HTTP</a>";
     BML::register_block( "DL", "DR", $dl );
 }
-
-BML::register_block( "METACTYPE", "S",
-    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' );
 
 1;
