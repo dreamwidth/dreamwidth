33ff75dab44ec9296265bca35552d1a12cd20c85
Kareila <kareila@dreamwidth.org>
2020-07-13 23:30:59 -0500

    convert /shop/receipt to TT

--- cgi-bin/DW/Controller/Shop.pm
+++ cgi-bin/DW/Controller/Shop.pm
@@ -33,6 +33,7 @@ DW::Routing->register_string( '/shop/points',         \&shop_points_handler,    
 DW::Routing->register_string( '/shop/icons',          \&shop_icons_handler,            app => 1 );
 DW::Routing->register_string( '/shop/transferpoints', \&shop_transfer_points_handler,  app => 1 );
 DW::Routing->register_string( '/shop/refundtopoints', \&shop_refund_to_points_handler, app => 1 );
+DW::Routing->register_string( '/shop/receipt',        \&shop_receipt_handler,          app => 1 );
 
 # our basic shop controller, this does setup that is unique to all shop
 # pages and everybody should call this first.  returns the same tuple as
@@ -451,6 +452,46 @@ sub shop_refund_to_points_handler {
     # Return the OK to the user.
     $rv->{refunded} = 1;
     return DW::Template->render_template( 'shop/refundtopoints.tt', $rv );
+}
+
+# view the receipt for a specific order
+sub shop_receipt_handler {
+
+    # this doesn't do form handling or state changes, don't need full shop_controller
+    my $r = DW::Request->get;
+    return $r->redirect("$LJ::SITEROOT/") unless LJ::is_enabled('payments');
+
+    if ( my $err = DW::Shop->remote_sysban_check ) {
+        return DW::Template->render_template( 'error.tt', { message => $err } );
+    }
+
+    my ( $ok, $rv ) = controller( anonymous => 1 );
+    return $rv unless $ok;
+
+    my $args  = $r->get_args;
+    my $scope = '/shop/receipt.tt';
+
+    # we don't have to be logged in, but we do need an ordernum passed in
+    my $ordernum = $args->{ordernum} // '';
+
+    my $cart = DW::Shop::Cart->get_from_ordernum($ordernum);
+    return error_ml("$scope.error.invalidordernum") unless $cart;
+
+    # cart cannot be in open, closed, or checkout state
+    my %invalid_state = (
+        $DW::Shop::STATE_OPEN     => 1,
+        $DW::Shop::STATE_CLOSED   => 1,
+        $DW::Shop::STATE_CHECKOUT => 1,
+    );
+    return $r->redirect("$LJ::SITEROOT/shop/cart") if $invalid_state{ $cart->state };
+
+    # set up variables for template
+    my $vars = { cart => $cart };
+
+    $vars->{orderdate} = DateTime->from_epoch( epoch => $cart->starttime );
+    $vars->{carttable} = LJ::Widget::ShopCart->render( receipt => 1, cart => $cart );
+
+    return DW::Template->render_template( 'shop/receipt.tt', $vars );
 }
 
 1;
