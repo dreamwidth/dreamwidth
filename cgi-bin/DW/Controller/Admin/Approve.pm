1bd13cc85724031fb011cf18d0661aff3fc8b7c0
Kareila <kareila@dreamwidth.org>
2020-08-03 21:46:49 -0500

    suspension form for /admin/recent_accounts
    Shows up to 10 accounts at a time and lets the viewer uncheck any accounts they don't think should be suspended.

--- cgi-bin/DW/Controller/Admin/Approve.pm
+++ cgi-bin/DW/Controller/Admin/Approve.pm
@@ -60,20 +60,25 @@ sub approve_handler {
     return DW::Template->render_template( 'admin/recent_accounts/review.tt', $vars )
         unless $r->did_post;
 
+    my $dbr = LJ::get_db_reader();
+    my $sth = $dbr->prepare( "SELECT userid FROM userproplite2 "
+            . "WHERE upropid=? AND value=? ORDER BY userid LIMIT ?" );
+
+    my $get_users = sub {
+        my ( $value, $limit ) = @_;
+        $sth->execute( $prop->{id}, $value, $limit );
+        die $dbr->errstr if $dbr->err;
+
+        my @uids;
+        push @uids, $_->[0] while $_ = $sth->fetchrow_arrayref;
+        return LJ::load_userids(@uids);    # hashref
+    };
+
     if ( $form_args->{begin} ) {
 
         # get a user to review - we don't want to just get the first result,
         # high chance of collision with multiple active reviewers
-        my @uids;
-        my $dbr = LJ::get_db_reader();
-        my $sth = $dbr->prepare( "SELECT userid FROM userproplite2 "
-                . "WHERE upropid=? AND value=? ORDER BY userid LIMIT 5" );
-        $sth->execute( $prop->{id}, 1 );
-        die $dbr->errstr if $dbr->err;
-        while ( $_ = $sth->fetchrow_arrayref ) {
-            push @uids, $_->[0];
-        }
-        my $users = LJ::load_userids(@uids);
+        my $users = $get_users->( 1, 5 );
 
         # make sure the accounts are at least an hour old - sorting
         # SELECT by userid means oldest accounts will be reviewed first
@@ -112,9 +117,8 @@ sub approve_handler {
         # reinforce the different colors for rejection vs approval
         $vars->{ $value ? 'errors' : 'success' } = $success;
 
-        # force lookup in case someone else just changed this -
-        # we still show the success message to the user, but
-        # we don't perform the actions again
+        # force lookup in case someone else just changed this - we still show
+        # the success message to the user, but don't perform the actions again
         delete $u->{"not_approved"};
         $u->preload_props( { use_master => 1 }, "not_approved" );
 
@@ -127,6 +131,45 @@ sub approve_handler {
             # $remote had suspend privs, but I think it's better to give the
             # user a chance to review their actions in case of fat fingering
         }
+
+        return DW::Template->render_template( 'admin/recent_accounts/review.tt', $vars );
+    }
+
+    if ( $form_args->{suspend} ) {
+        return $r->redirect("$LJ::SITEROOT/admin/recent_accounts")
+            unless $vars->{can_suspend};
+
+        # get a list of flagged users to review for suspension
+        $vars->{users} = $get_users->( 2, 10 );
+
+        return DW::Template->render_template( 'admin/recent_accounts/suspend.tt', $vars );
+    }
+
+    if ( $form_args->{do_suspend} ) {
+        my @ids = split / /, $form_args->{uids};
+        return error_ml('error.invalidform') unless @ids && $vars->{can_suspend};
+
+        my $users   = LJ::load_userids(@ids);
+        my $success = DW::FormErrors->new;
+        my $count   = 0;
+
+        foreach my $uid (@ids) {
+            my $u = $users->{$uid};
+            next if $u->is_suspended;    # already done
+
+            if ( $form_args->{"user_$uid"} ) {
+                $u->set_suspended( $remote, "from /admin/recent_accounts" );
+                $count++;
+            }
+            else {
+                $u->set_prop( not_approved => 0 );
+                LJ::statushistory_add( $u, $remote, 'approvenew',
+                    sprintf( "new account %s approved by %s", $u->user, $remote->user ) );
+            }
+        }
+
+        $success->add( '', '.success.suspend', { count => $count } );
+        $vars->{success} = $success;
 
         return DW::Template->render_template( 'admin/recent_accounts/review.tt', $vars );
     }
