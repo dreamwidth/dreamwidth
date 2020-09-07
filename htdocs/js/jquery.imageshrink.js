c90a58c26dfc27d7daaa68b1f50953652e3264a8
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-09-07 13:45:46 -0700

    The Squish, Part Four
    The quest to perfect this image-shrinking behavior continues. This time:
    
    - Distinguish between "normal" and "tall" (height > 2x width) casual images, and
    let tall images scroll instead of fitting the viewport. Nicer un-zoomed state
    for vertical comic strips.
    
    - Any image with a `style="..."` attribute is exempt from the squish. Same
    logic as the decorative code exemption: inline styles means you're spending
    effort to make things Just So, and the only point of the squish is to save your
    time when you _don't_ want to spend effort. (Sidenote: we can't use the
    width/height attributes as this kind of signal, because casual "share" copypasta
    often has large w/h values.)
    
    - Use img width/height attributes as the maximum size for the _zoomed-in_ state.
    Nicer zoom behavior when using an over-resolution image to look good on retina
    screens.

--- htdocs/js/jquery.imageshrink.js
+++ htdocs/js/jquery.imageshrink.js
@@ -3,6 +3,7 @@
 // - Zooming (on click)
 // - Exempting decorative markup from Squish (should be pure CSS, but can't yet)
 // - Hiding zoom cursors for images that won't zoom
+// - Marking tall images so they get modified squish behavior
 jQuery(function($) {
 
     // First: Basic click-to-zoom. (.imageshrink-expanded on/off)
@@ -35,25 +36,56 @@ jQuery(function($) {
     // Check the whole document to start with.
     protectTheCodes(document);
 
-    // Third: Don't show zoom cursors for non-zooming images (.imageshrink-actualsize on/off)
-
-    // Dummied out observeImages function, to simplify browser support in (Fourth).
+    // Dummied out observeImages function, to simplify browser support in (Fifth).
     var observeImages = function(container) {return;};
 
     if (typeof ResizeObserver === 'function') {
-        var zoomCursorCleaner = new ResizeObserver(function(resizeList, observer) {
+        var imageShrinkResizeObserver = new ResizeObserver(function(resizeList, observer) {
             resizeList.forEach(function(entry) {
                 var img = entry.target;
                 if (img.tagName !== 'IMG') { return; }
 
-                // Zoomed images don't count.
-                if ( ! img.classList.contains('imageshrink-expanded')
-                    && img.width === img.naturalWidth
-                    && img.height === img.naturalHeight
-                ) {
+                // Third: Skip zoom cursors for images that won't zoom (.imageshrink-actualsize on/off)
+                // "Won't zoom" means it's in its "shrunk" state and is already
+                // either "natural" full size or "requested" full size (as per
+                // the height/width attributes).
+                // "Natural" is easy:
+                var isNaturalSize = img.width === img.naturalWidth && img.height === img.naturalHeight;
+                // "Requested" is hard. `object-fit: contain` w/ max-height means
+                // layout width might be wider than visible width. There's no way
+                // to directly measure visible width, and of course that's the one
+                // we care about. Plus maybe they only set one attribute. So we
+                // measure indirectly: if the layout aspect ratio doesn't match the
+                // natural aspect ratio (+/- some slop bc floats), it CAN'T be the
+                // requested size. (And at least one dimension has to match.)
+                // And yes, this ignores the use case of deliberately mutilating the
+                // aspect ratio for comedy; use something other than the height/width
+                // attrs for that.
+                var attrWidth = parseInt(img.getAttribute('width'));
+                var attrHeight = parseInt(img.getAttribute('height'));
+                var isRequestedSize =
+                    (    ( !isNaN(attrWidth) && attrWidth === img.width )
+                      || ( !isNaN(attrHeight) && attrHeight === img.height )
+                    )
+                    && Math.abs( (img.height / img.width) - (img.naturalHeight / img.naturalWidth) ) < 0.004;
+                    // Might want to tune that slop later. Or hey, maybe I nailed it. -NF
+
+                var isActualSize = isNaturalSize || isRequestedSize;
+
+                if ( isActualSize && ! img.classList.contains('imageshrink-expanded') ) {
                     img.classList.add('imageshrink-actualsize');
                 } else {
                     img.classList.remove('imageshrink-actualsize');
+                }
+
+                // Fourth: Mark tall images so we can let them scroll. Doing
+                // this in the ResizeObserver because images have unknown natural
+                // aspect ratios until load & decode; they'll fire a resize once
+                // the browser sorts it out.
+                if ( (img.naturalHeight / img.naturalWidth) >= 2 ) {
+                    img.classList.add('imageshrink-tall');
+                } else {
+                    img.classList.remove('imageshrink-tall');
                 }
             });
         });
@@ -63,7 +95,7 @@ jQuery(function($) {
             let images = container.querySelectorAll('.entry-content img, .comment-content img');
             // Anything with ResizeObserver definitely has NodeList.forEach.
             images.forEach(function(img) {
-                zoomCursorCleaner.observe(img);
+                imageShrinkResizeObserver.observe(img);
             });
         }
 
@@ -71,7 +103,7 @@ jQuery(function($) {
         observeImages(document);
     }
 
-    // Fourth: Repeat (Second) and (Third) when adding images to the page.
+    // Fifth: Repeat (Second), (Third), (Fourth) when adding images to the page.
     // (Via cut tags, comment expansion, or image placeholders.)
     if (typeof MutationObserver === 'function') {
         var imageUpdater = new MutationObserver(function(mutationList, observer) {
