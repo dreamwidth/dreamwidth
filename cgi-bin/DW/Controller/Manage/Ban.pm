62051c18a9bc212a42c549face705faf94b9ecf3
Cocoa <momijizukamori@gmail.com>
2022-04-27 15:04:37 +0000

    Run tidy

--- cgi-bin/DW/Controller/Manage/Ban.pm
+++ cgi-bin/DW/Controller/Manage/Ban.pm
@@ -32,25 +32,26 @@ sub ban_handler {
 
     my $r = DW::Request->get;
 
-    my $u         = $rv->{remote};
-    my $POST   = $r->post_args;
-    my $GET    = $r->get_args;
+    my $u    = $rv->{remote};
+    my $POST = $r->post_args;
+    my $GET  = $r->get_args;
 
     my $submit_msg = 0;
     my %editvals;
 
-    if ($r->did_post) {
+    if ( $r->did_post ) {
+
         # check to see if we're doing a note edit instead
         foreach ( keys %{$POST} ) {
-            my ( $euid ) =  /^edit_ban_(\d+)$/;
+            my ($euid) = /^edit_ban_(\d+)$/;
             if ( defined $euid && $POST->{"edit_ban_$euid"} ) {
-                my $eu = LJ::load_userid( $euid );
+                my $eu = LJ::load_userid($euid);
                 last unless $eu;
                 %editvals = (
                     user => $eu->user,
-                    note => $u->ban_note( $eu )->{$euid},
+                    note => $u->ban_note($eu)->{$euid},
                 );
-                last;  # stop searching keys
+                last;    # stop searching keys
             }
         }
 
@@ -61,12 +62,13 @@ sub ban_handler {
 
         # unban users
         if ( $POST->{unban_user} && !%editvals ) {
+
             # first remove any users from the list that are not valid users
-            my @unbanlist = split(/\0/, $POST->{unban_user});
-            my $unbanus = LJ::load_userids(@unbanlist);
-            for (my $i = 0; $i < scalar @unbanlist; $i++) {
-                unless ($unbanus->{$unbanlist[$i]}) {
-                    splice(@unbanlist, $i, 1);
+            my @unbanlist = split( /\0/, $POST->{unban_user} );
+            my $unbanus   = LJ::load_userids(@unbanlist);
+            for ( my $i = 0 ; $i < scalar @unbanlist ; $i++ ) {
+                unless ( $unbanus->{ $unbanlist[$i] } ) {
+                    splice( @unbanlist, $i, 1 );
                     $i--;
                 }
             }
@@ -76,13 +78,15 @@ sub ban_handler {
         }
 
         # ban users
-        if ( %editvals ) {
-            $r->add_msg(LJ::Lang::ml('/manage/banusers.tt.editmsg'), $r->WARNING); 
+        if (%editvals) {
+            $r->add_msg( LJ::Lang::ml('/manage/banusers.tt.editmsg'), $r->WARNING );
             $submit_msg = 1;
-        } elsif ( $POST->{ban_list} ) {
+        }
+        elsif ( $POST->{ban_list} ) {
+
             # first remove any users from the list that are not valid users
             # FIXME: we need load_user_multiple
-            my @banlist_orig = split(/,/, $POST->{ban_list});
+            my @banlist_orig = split( /,/, $POST->{ban_list} );
             my @banlist;
             foreach my $banusername (@banlist_orig) {
                 my $banu = LJ::load_user_or_identity($banusername);
@@ -90,11 +94,12 @@ sub ban_handler {
             }
 
             # make sure the user isn't over the max number of bans allowed
-            my $banned = LJ::load_rel_user($u, 'B') || [];
-            if (scalar @$banned >= ($LJ::MAX_BANS || 5000)) {
-                $r->add_msg(LJ::Lang::ml('/manage/banusers.tt.error.toomanybans'), $r->ERROR); 
+            my $banned = LJ::load_rel_user( $u, 'B' ) || [];
+            if ( scalar @$banned >= ( $LJ::MAX_BANS || 5000 ) ) {
+                $r->add_msg( LJ::Lang::ml('/manage/banusers.tt.error.toomanybans'), $r->ERROR );
                 $submit_msg = 1;
-            } else {
+            }
+            else {
                 # now ban the users
                 $u->ban_user_multi(@banlist) if @banlist;
             }
@@ -104,9 +109,8 @@ sub ban_handler {
             }
         }
 
-        $r->add_msg(LJ::Lang::ml('/manage/banusers.tt.success'), $r->SUCCESS) unless $submit_msg;
+        $r->add_msg( LJ::Lang::ml('/manage/banusers.tt.success'), $r->SUCCESS ) unless $submit_msg;
     }
-
 
     # because we may have input from multiple community admins
     # separate the old note from the new note;
@@ -115,29 +119,29 @@ sub ban_handler {
 
     my $banned = $u->banned_userids;
     my @banned_array;
-    if ($banned && @$banned) {
-        my $us = LJ::load_userids(@$banned);
-        my $notes = $u->ban_note( $banned );
+    if ( $banned && @$banned ) {
+        my $us    = LJ::load_userids(@$banned);
+        my $notes = $u->ban_note($banned);
 
         foreach my $banuid (@$banned) {
             my $bu = $us->{$banuid};
             next unless $bu;
             my $note = $notes->{$banuid} || '';
             LJ::CleanHTML::clean_subject( \$note );
-            $note = LJ::html_newlines( $note );
-            push @banned_array, {user => $bu, banuid => $banuid, note => $note};
+            $note = LJ::html_newlines($note);
+            push @banned_array, { user => $bu, banuid => $banuid, note => $note };
 
         }
     }
 
     my $vars = {
-        banned => $banned,
-        banned_array    => \@banned_array,
+        banned       => $banned,
+        banned_array => \@banned_array,
 
         separate_add_from_edit => $separate_add_from_edit,
-        u      => $u,
-        authas_html => $rv->{authas_html},
-        editvals => \%editvals,
+        u                      => $u,
+        authas_html            => $rv->{authas_html},
+        editvals               => \%editvals,
     };
 
     return DW::Template->render_template( 'manage/banusers.tt', $vars );
