d22a88202fd3c59cc77fd76badf43581093cff0c
Carly Ho <carlymho@fastmail.com>
2023-06-16 20:22:02 -0500

    Add ffnet user icon to repository

--- cgi-bin/DW/External/Site/FanFiction.pm
+++ cgi-bin/DW/External/Site/FanFiction.pm
@@ -63,7 +63,7 @@ sub badge_image {
 
     # for lack of anything better, let's use the favicon
     return {
-        url    => "https://www.fanfiction.net/static/icons3/ff-icon-192.png",
+        url    => "/img/userheads/ff-icon-192.png",
         width  => 16,
         height => 16,
     };
