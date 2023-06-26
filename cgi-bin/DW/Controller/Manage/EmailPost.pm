728735e4e745bfd6b7acc1052b61a8b3f6f291b9
Kareila <kareila@dreamwidth.org>
2023-06-26 01:30:05 -0500

    add form processing to cgi-bin/DW/Controller/Manage/EmailPost.pm

--- cgi-bin/DW/Controller/Manage/EmailPost.pm
+++ cgi-bin/DW/Controller/Manage/EmailPost.pm
@@ -28,9 +28,11 @@ use strict;
 use DW::Controller;
 use DW::Routing;
 use DW::Template;
+use DW::FormErrors;
 
 use DW::API::Key;
 use LJ::Emailpost::Web;
+use LJ::Setting::EmailPosting;
 
 DW::Routing->register_string( "/manage/emailpost", \&emailpost_handler, app => 1 );
 
@@ -47,13 +49,12 @@ sub emailpost_handler {
 
     return error_ml("$ml_scope.error.acct") unless $u->can_emailpost;
 
-    $u->preload_props(
-        qw/
-            emailpost_pin emailpost_allowfrom
-            emailpost_userpic emailpost_security
-            emailpost_comments emailpost_gallery
-            /
-    );
+    my @emailpost_props = qw/
+        emailpost_pin emailpost_allowfrom
+        emailpost_userpic emailpost_security
+        emailpost_comments
+        /;
+    $u->preload_props(@emailpost_props);
     $rv->{u} = $u;
 
     my $r         = $rv->{r};
@@ -65,7 +66,7 @@ sub emailpost_handler {
         $rv->{type}      = $type;
         $rv->{format_to} = sub { sprintf "%s@%s", $_[0], $LJ::EMAIL_POST_DOMAIN };
 
-        if ( my @addr = split /\s*,\s*/, $u->{emailpost_allowfrom} ) {
+        if ( my @addr = split /\s*,\s*/, ( $u->{emailpost_allowfrom} || '' ) ) {
             my $email = $addr[0];
             $email =~ s/\(\w\)$//;
             $rv->{example} = $email;
@@ -74,13 +75,134 @@ sub emailpost_handler {
         return DW::Template->render_template( 'manage/emailpost_help.tt', $rv );
     }
 
-    if ( $r->did_post && $form_args->{save} ) {
-
-        # COME BACK TO THIS LATER
-    }
+    # this was 2, but it's 4 on mobile settings - should be consistent
+    $rv->{addr_max} = 4;
 
     $rv->{addrlist} = LJ::Emailpost::Web::get_allowed_senders($u);
     $rv->{apikeys}  = DW::API::Key->get_keys_for_user($u);
+
+    if ( $r->did_post ) {
+        my $success_links = [
+            {
+                text => LJ::Lang::ml("$ml_scope.success.back"),
+                url  => '/manage/emailpost'
+            },
+            {
+                text => LJ::Lang::ml("$ml_scope.success.info"),
+                url  => '/manage/emailpost?mode=help'
+            },
+            {
+                text => LJ::Lang::ml("$ml_scope.success.settings"),
+                url  => '/manage/settings/?cat=mobile'
+            },
+        ];
+
+        my $errors = DW::FormErrors->new;
+
+        if ( $form_args->{save} ) {
+            my $pin = $form_args->{pin};
+            $pin =~ s/\s+//g if defined $pin;
+
+            $errors->add( 'pin', "$ml_scope.error.invalidpin", { num => 4 } )
+                if $pin && $pin !~ /^([a-z0-9]){4,20}$/i;
+
+            $errors->add( 'pin', "$ml_scope.error.invalidpinuser" )
+                if $pin && $pin eq $u->user;
+
+            # Check email, add flags if needed.
+            my %allowed;
+            my @send_helpmessage;
+
+            foreach my $count ( 0 .. $rv->{addr_max} ) {
+                my $a = $form_args->{"addresses_$count"} or next;
+                $a =~ s/\s+//g;
+                next unless $a;
+                next if length $a > 80;
+                $a = lc $a;
+
+                my @email_errors;
+                LJ::check_email( $a, \@email_errors, { force_spelling => 1 } );
+
+                $errors->add(
+                    "addresses_$count",
+                    "$ml_scope.error.invalidemail",
+                    { email => LJ::ehtml($a), error => $email_errors[0] }
+                ) if @email_errors;
+
+                $allowed{$a} = {};
+                $allowed{$a}->{get_errors} = 1 if $form_args->{"check_$count"};
+                push @send_helpmessage, $a if $form_args->{"help_$count"};
+            }
+
+            if ( $errors->exist ) {
+                $rv->{errors}   = $errors;
+                $rv->{formdata} = $r->post_args;
+                return DW::Template->render_template( 'manage/emailpost.tt', $rv );
+            }
+
+            $u->set_prop( "emailpost_pin", $pin );
+            foreach my $prop (@emailpost_props) {
+                next if $prop =~ /emailpost_(allowfrom|pin)/;
+                next if ( $u->{$prop} // '' ) eq ( $form_args->{$prop} // '' );
+
+                if ( $form_args->{$prop} && $form_args->{$prop} ne 'default' ) {
+                    $u->set_prop( $prop, $form_args->{$prop} );
+                }
+                else {
+                    $u->set_prop( $prop, undef );
+                }
+            }
+
+            LJ::Emailpost::Web::set_allowed_senders( $u, \%allowed );
+            LJ::Setting::EmailPosting->email_helpmessage( $u, $_ ) foreach @send_helpmessage;
+
+            return success_ml( "$ml_scope.success.message", undef, $success_links );
+        }
+
+        my $info_box = sub {
+            my ( $bool, $succ, $err ) = @_;
+            my @sp_args = $bool ? ( 'highlight', $succ ) : ( 'error', $err );
+            return sprintf "<div class='%s-box'>%s</div>", @sp_args;
+        };
+
+        $rv->{info_box} = '';
+
+        # if we just reset the token, add a header but show the original page still
+        if ( $form_args->{'action:token'} ) {
+            $rv->{info_box} = $info_box->(
+                $u->generate_emailpost_auth,
+                LJ::Lang::ml("$ml_scope.reply.status.success"),
+                LJ::Lang::ml("$ml_scope.reply.status.error"),
+            );
+        }
+
+        if ( $form_args->{'action:apikey'} ) {
+            $rv->{info_box} = $info_box->(
+                DW::API::Key->new_for_user($u),
+                LJ::Lang::ml("$ml_scope.api.status.success"),
+                LJ::Lang::ml("$ml_scope.api.status.error"),
+            );
+        }
+
+        if ( $form_args->{'action:delete'} ) {
+            for ( my $api_idx = 0 ; $api_idx < @{ $rv->{apikeys} } ; $api_idx++ ) {
+                my $deleted = $form_args->{"delete_${api_idx}"} or next;
+                $deleted =~ s/\s+//g;
+                next unless $deleted;
+                my $key = DW::API::Key->get_key($deleted);
+                next unless defined $key;
+
+                $rv->{info_box} .= $info_box->(
+                    $key->delete($u),
+                    LJ::Lang::ml( "$ml_scope.api.delete.success", { key => $key->{keyhash} } ),
+                    LJ::Lang::ml( "$ml_scope.api.delete.error",   { key => $key->{keyhash} } ),
+                );
+            }
+        }
+
+        # regenerate list to reflect key changes
+        $rv->{apikeys} = DW::API::Key->get_keys_for_user($u);
+    }
 
     return DW::Template->render_template( 'manage/emailpost.tt', $rv );
 }
