a8a9055c66262b809670b7f3e66639456de973d8
Kareila <kareila@dreamwidth.org>
2023-07-25 10:48:19 -0500

    use friends_link format instead of viewall format for "more" link

--- cgi-bin/LJ/Widget/FriendBirthdays.pm
+++ cgi-bin/LJ/Widget/FriendBirthdays.pm
@@ -41,11 +41,10 @@ sub render_body {
     return "" unless @bdays;
 
     my $vars = {
-        bdays        => \@bdays,
-        load_user    => \&LJ::load_user,
-        month_short  => \&LJ::Lang::month_short,
-        friends_link => $opts{friends_link},
-        u            => $u
+        bdays       => \@bdays,
+        load_user   => \&LJ::load_user,
+        month_short => \&LJ::Lang::month_short,
+        u           => $u
     };
 
     return DW::Template->template_string( 'widget/friendbirthdays.tt', $vars );
