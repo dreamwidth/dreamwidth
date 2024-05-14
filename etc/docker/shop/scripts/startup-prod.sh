75536edad63f8bff62da41f2b6e6f4494ff28ccc
Mark Smith <mark@plogs.net>
2024-05-14 12:00:56 -0700

    Split shop out to ubuntu 22.04 and pray

---
+++ etc/docker/shop/scripts/startup-prod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xe
+
+# Validate that the system is set up and working correctly. If this fails, sleep forever
+# so that someone can log in and debug.
+perl -I$LJHOME/extlib/ $LJHOME/bin/checkconfig.pl || sleep infinity
+
+# Kick off Apache
+mkdir $LJHOME/ext/local/etc/apache2/sites-enabled || true
+cp $LJHOME/ext/local/dreamwidth-prod.conf $LJHOME/ext/local/etc/apache2/sites-enabled/dreamwidth.conf
+trap "cat /var/log/apache2/error.log 1>&2" ERR
+/usr/sbin/apache2ctl configtest
+/usr/sbin/apache2ctl start
+trap - ERR
+
+# Kick off Varnish
+service varnish start
+
+# Sleep a few seconds to ensure things get up and running
+sleep 5
+
+# Now we "wait" by tailing the error log, so we can see it without having
+# to attach to the container
+tail -F /var/log/apache2/error.log
